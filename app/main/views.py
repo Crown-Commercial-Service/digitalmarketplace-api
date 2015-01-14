7d8889ddb66f00ed5a9228c0c145d507ea09cc26
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-01-14 18:46:15 +0000

    Adds most CRUD functionality to services resource
    Adds views to list existing services, create new ones and update existing
    ones. It does not add the ability to delete an existing service as this
    is not yet needed.
    
    In doing this a few things have changed.
    
    I have changed the error handlers to return JSON and removed the error
    handler html templates. I have done this because at the moment we are
    building a JSON API, so our error responses should also be in JSON. I
    have also replaced the root view with one that returns a JSON object
    that links to the /services resource and removed it's template. We have
    an intention to build a hypermedia API, part of that is clients being
    able to discover the resources they can interact with on their own.
    
    I have refactored the tests to pull application tests into their own
    package and split tests on the /services resource out into their own
    module. I've done this to better reflect the structure of the
    application in the tests and because the prevous test module was getting
    quite large. One strange thing I have done is to use a mixin to provide
    some common tests to each endpoint that accepts JSON data. We will see
    if this is a model that works well.
    
    I have NOT done any validation of the services JSON itself. We have
    another story going on to validate against a JSON schema.

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template, jsonify, Response
+from flask import render_template, jsonify, request, abort, url_for, Response
 
 from . import main
+from .. import db
 from ..lib.authentication import requires_authentication
 from ..models import Service
 
@@ -8,7 +9,13 @@ from ..models import Service
 @main.route('/')
 @requires_authentication
 def index():
-    return render_template('main.html')
+    """Entry point for the API, show the resources that are available."""
+    return jsonify(links=[
+        {
+            "rel": "services.list",
+            "href": url_for('.list_services', _external=True)
+        }
+    ]), 200
 
 
 @main.route('/services/g6-scs-example')
@@ -47,8 +54,67 @@ def get_iaas():
     return resp
 
 
-@main.route('/services/<id>')
-def get_service(id):
-    service = Service.query.filter(Service.id == id).first_or_404()
+@main.route('/services', methods=['GET'])
+@requires_authentication
+def list_services():
+    return jsonify(services=map(jsonify_service, Service.query.all()))
 
-    return jsonify(id=id, data=service.data)
+
+@main.route('/services', methods=['POST'])
+@requires_authentication
+def add_service():
+    data = get_json_from_request()
+
+    service = Service(data=data['services'])
+    db.session.add(service)
+    db.session.commit()
+
+    return jsonify(services=jsonify_service(service)), 201
+
+
+@main.route('/services/<service_id>', methods=['PUT'])
+@requires_authentication
+def update_service(service_id):
+    service = Service.query.filter(Service.id == service_id).first_or_404()
+    data = get_json_from_request()
+    if 'id' in data['services']:
+        if data['services'].pop('id') != service.id:
+            abort(400, "Invalid service ID provided")
+
+    service.data = data['services']
+
+    db.session.add(service)
+    db.session.commit()
+
+    return jsonify(services=jsonify_service(service)), 200
+
+
+
+@main.route('/services/<service_id>', methods=['GET'])
+@requires_authentication
+def get_service(service_id):
+    service = Service.query.filter(Service.id == service_id).first_or_404()
+
+    return jsonify(services=jsonify_service(service))
+
+
+def jsonify_service(service):
+    data = dict(service.data.items())
+    data['id'] = service.id
+
+    data['links'] = [
+        {"rel": "self", "href": url_for('.get_service', service_id=service.id, _external=True)}
+    ]
+    return data
+
+
+def get_json_from_request():
+    if request.content_type != 'application/json':
+        abort(400, "Unexpected Content-Type, expecting 'application/json'")
+    data = request.get_json()
+    if data is None:
+        abort(400, "Invalid JSON; must be a valid JSON object")
+    if 'services' not in data:
+        abort(400, "Invalid JSON must have a 'services' key")
+
+    return data
