0b6e18516846f6712315dae3a9d844193936f278
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-01-14 16:06:11 +0000

    [KK] Serve static example JSON files for each lot

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, Response
 
 from . import main
 from ..lib.authentication import requires_authentication
@@ -9,6 +9,42 @@ from ..models import Service
 @requires_authentication
 def index():
     return render_template('main.html')
+
+
+@main.route('/services/g6-scs-example')
+def get_scs():
+    content = render_template('sample_static_responses/sample-scs.json')
+    resp = Response(response=content,
+                    status=200,
+                    mimetype="application/json")
+    return resp
+
+
+@main.route('/services/g6-saas-example')
+def get_saas():
+    content = render_template('sample_static_responses/sample-saas.json')
+    resp = Response(response=content,
+                    status=200,
+                    mimetype="application/json")
+    return resp
+
+
+@main.route('/services/g6-paas-example')
+def get_paas():
+    content = render_template('sample_static_responses/sample-paas.json')
+    resp = Response(response=content,
+                    status=200,
+                    mimetype="application/json")
+    return resp
+
+
+@main.route('/services/g6-iaas-example')
+def get_iaas():
+    content = render_template('sample_static_responses/sample-iaas.json')
+    resp = Response(response=content,
+                    status=200,
+                    mimetype="application/json")
+    return resp
 
 
 @main.route('/services/<id>')
