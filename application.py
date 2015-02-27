a09edcdf11c0d6c6b43cbff5029ac8cfb5741170
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-02-27 17:17:06 +0000

    Update to run on port 5000
    For development we will want to run multiple apps, so they should each bind to a different port number.
    The default port is 5000 anyway, but we should state the port explicitly in the code which is why I've added it here.

--- application.py
+++ application.py
@@ -2,11 +2,12 @@
 
 import os
 from app import create_app, db
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Server
 from flask.ext.migrate import Migrate, MigrateCommand
 
 application = create_app(os.getenv('FLASH_CONFIG') or 'development')
 manager = Manager(application)
+manager.add_command("runserver", Server(port=5000))
 migrate = Migrate(application, db)
 manager.add_command('db', MigrateCommand)
 
