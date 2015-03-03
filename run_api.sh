efa23f00cd7bddfae2f0910c58349d2e1e35d882
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-03-03 12:40:40 +0000

    Update run scripts to set missing environment variables
    Builds on Travis (and anywhere else without the environment set up) will currently fail.  By setting sensible defaults in the run scripts it allows tests to pass and gives developers the convenience of only needing to run one script (per app) to get things working locally.

---
+++ run_api.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source ./venv/bin/activate 2>/dev/null && echo "Virtual environment activated."
+
+# Use default environment vars for localhost if not already set
+export DM_AUTH_TOKENS=${DM_AUTH_TOKENS:=myToken}
+
+echo "Environment variables in use:" 
+env | grep DM_
+
+python application.py runserver
