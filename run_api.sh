def2740e6eea36c5a32eba91061f6101e394d590
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-03-03 10:42:23 +0000

    Update run script
    Modified to only set the AUTH_TOKENS environment variable if it has not already been set.

---
+++ run_api.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source ./venv/bin/activate
+echo "Virtual environment activated."
+
+# Use default environment vars for localhost if not already set
+export AUTH_TOKENS=${AUTH_TOKENS:=myToken}
+
+echo "Environment variables in use:" 
+env | grep AUTH_TOKENS
+
+python application.py runserver
