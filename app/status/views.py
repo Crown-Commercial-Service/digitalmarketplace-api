de46dcf369277e4a80387ffd93adc9794ab22ccd
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-03-23 17:43:51 +0000

    Add version to the /_status endpoint
    This is pulled from a file in the root of the application which is
    placed by the release tool.

--- app/status/views.py
+++ app/status/views.py
@@ -1,8 +1,9 @@
 from flask import jsonify
 
 from . import status
+from .. import utils
 
 
 @status.route('/_status')
 def status_no_db():
-    return jsonify(status="ok")
+    return jsonify(status="ok", version=utils.get_version_label())
