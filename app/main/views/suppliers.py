b4d85e9a48022ef82f59e01cabe3fa7cb44ffea7
Paul Craig <paul.craig@digital.cabinet-office.gov.uk>
2015-03-30 16:54:01 +0100

    Removed row2dict method in favour of `jsonify_supplier`

--- app/main/views/suppliers.py
+++ app/main/views/suppliers.py
@@ -10,18 +10,7 @@ def get_supplier(supplier_id):
         Supplier.supplier_id == supplier_id
     ).first_or_404()
 
-    return jsonify(suppliers=row2dict(supplier))
-
-
-def row2dict(row):
-    """
-    Creates a dictionary object from the {column_name}:{value} pairs
-    of an sqlAlchemy query object
-    @see http://stackoverflow.com/questions/1958219/
-            convert-sqlalchemy-row-object-to-python-dict#comment12704946_1960546
-    """
-    return dict((col, getattr(row, col))
-                for col in row.__table__.columns.keys())
+    return jsonify(suppliers=jsonify_supplier(supplier))
 
 
 def jsonify_supplier(supplier):
