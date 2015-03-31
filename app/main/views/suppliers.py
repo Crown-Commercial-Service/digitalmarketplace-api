06c0fc6dc7e01d62b93a3be0f65893321af83e2f
Paul Craig <paul.craig@digital.cabinet-office.gov.uk>
2015-03-31 12:29:00 +0100

    Starting `prefix` list route.

--- app/main/views/suppliers.py
+++ app/main/views/suppliers.py
@@ -1,7 +1,32 @@
 from flask import jsonify
+from flask import request
 
 from .. import main
 from ...models import Supplier
+
+
+@main.route('/suppliers', methods=['GET'])
+def get_suppliers_by_prefix():
+
+    prefix = request.args.get('prefix', '')
+
+    if not prefix:
+        return '<h1>Oops, please include a query with a `prefix` key</h1>'
+
+    suppliers = Supplier.query.filter(
+        Supplier.name.ilike(prefix + '%')
+    ).all()
+
+    if not suppliers:
+        return '<h1>Oops, no suppliers found with names that start with "' \
+               + prefix + '"</h1>'
+
+    suppliers_json = []
+
+    for supplier in suppliers:
+        suppliers_json.append(jsonify_supplier(supplier))
+
+    return jsonify(suppliers=suppliers_json)
 
 
 @main.route('/suppliers/<int:supplier_id>', methods=['GET'])
