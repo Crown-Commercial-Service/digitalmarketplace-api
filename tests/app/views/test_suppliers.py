c9a9970be63ff79654b5355ed533792a7af59eb1
Paul Craig <paul.craig@digital.cabinet-office.gov.uk>
2015-03-31 16:10:29 +0100

    Unit tests for returning supplier lists based on prefix.

--- tests/app/views/test_suppliers.py
+++ tests/app/views/test_suppliers.py
@@ -30,3 +30,60 @@ class TestGetSupplier(BaseApplicationTest):
         assert_equal(200, response.status_code)
         assert_equal(585274, data['suppliers']['id'])
         assert_equal(u"Supplier 585274", data['suppliers']['name'])
+
+
+class TestGetSuppliers(BaseApplicationTest):
+    def setup(self):
+        super(TestGetSuppliers, self).setup()
+
+        with self.app.app_context():
+            db.session.add(
+                Supplier(supplier_id=585274, name=u"Supplier 585274")
+            )
+            db.session.add(
+                Supplier(supplier_id=123456, name=u"Supplier 123456")
+            )
+            db.session.add(
+                Supplier(
+                    supplier_id=3,
+                    name=u"Cloudy Clouds Inc Clouded Hosting"
+                )
+            )
+
+    def test_query_string_missing(self):
+        response = self.client.get('/suppliers')
+        assert_equal(400, response.status_code)
+
+    def test_query_string_prefix_empty(self):
+        response = self.client.get('/suppliers?prefix=')
+        assert_equal(400, response.status_code)
+
+    def test_query_string_prefix_returns_none(self):
+        response = self.client.get('/suppliers?prefix=canada')
+        assert_equal(404, response.status_code)
+
+    def test_query_string_prefix_sql_injection_wont_work(self):
+        response = \
+            self.client.get('/suppliers?prefix=;DROP%20TABLE%20suppliers;')
+        assert_equal(404, response.status_code)
+
+    def test_query_string_prefix_returns_single(self):
+        response = self.client.get('/suppliers?prefix=cloud')
+
+        data = json.loads(response.get_data())
+        assert_equal(200, response.status_code)
+        assert_equal(1, len(data['suppliers']))
+        assert_equal(3, data['suppliers'][0]['id'])
+        assert_equal(
+            u"Cloudy Clouds Inc Clouded Hosting",
+            data['suppliers'][0]['name']
+        )
+
+    def test_query_string_prefix_returns_multiple(self):
+        response = self.client.get('/suppliers?prefix=s')
+
+        data = json.loads(response.get_data())
+        assert_equal(200, response.status_code)
+        assert_equal(2, len(data['suppliers']))
+        assert_equal(585274, data['suppliers'][0]['id'])
+        assert_equal(u"Supplier 123456", data['suppliers'][1]['name'])
