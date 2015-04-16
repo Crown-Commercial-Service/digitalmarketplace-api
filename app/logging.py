d8bae51f96043446edff5412b0255dc2afd4d9c0
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-04-16 22:06:57 +0100

    Add application logging
    - Adds the application name to the log entries
    - Adds a request ID to the log entries
    - Uses ISO 8601 as the date time format
    - Adds request ID to response

---
+++ app/logging.py
@@ -0,0 +1,87 @@
+from __future__ import absolute_import
+import logging
+import uuid
+
+from flask import g, request
+from flask.ctx import has_app_context
+
+LOG_FORMAT = '%(asctime)s %(app_name)s %(levelname)s %(request_id)s: ' \
+             '%(message)s [in %(pathname)s:%(lineno)d]'
+
+
+def init_app(app):
+    app.config.setdefault('DM_LOG_LEVEL', 'INFO')
+    app.config.setdefault('DM_APP_NAME', 'none')
+    app.config.setdefault('DM_LOG_PATH', './log/application.log')
+    app.config.setdefault('DM_REQUEST_ID_HEADER', 'DM-Request-ID')
+    app.config.setdefault('DM_DOWNSTREAM_REQUEST_ID_HEADER', '')
+
+    handler = get_handler(app)
+
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging_level(app.config['DM_LOG_LEVEL']))
+
+    request_id_header = app.config['DM_REQUEST_ID_HEADER']
+    downstream_header = app.config['DM_DOWNSTREAM_REQUEST_ID_HEADER']
+
+    @app.before_request
+    def before_request():
+        g.request_id = get_request_id(request,
+                                      request_id_header, downstream_header)
+
+    @app.after_request
+    def after_request(response):
+        response.headers[request_id_header] = g.request_id
+        return response
+
+
+def get_handler(app):
+    formatter = CustomFormatter(LOG_FORMAT, app.config['DM_APP_NAME'])
+
+    handler = logging.FileHandler(app.config['DM_LOG_PATH'])
+    handler.setLevel(logging_level(app.config['DM_LOG_LEVEL']))
+    handler.setFormatter(formatter)
+
+    return handler
+
+
+def logging_level(name):
+    """Return logging level number from name
+
+    >>> logging_level('DEBUG')
+    10
+    """
+    return logging._levelNames[name]
+
+
+def get_request_id(request, request_id_header, downstream_header):
+    if request_id_header in request.headers:
+        return request.headers.get(request_id_header)
+    elif downstream_header and downstream_header in request.headers:
+        return request.headers.get(downstream_header)
+    else:
+        return str(uuid.uuid4())
+
+
+class CustomFormatter(logging.Formatter):
+    def __init__(self, log_format, app_name):
+        super(CustomFormatter, self).__init__(log_format,
+                                              '%Y-%m-%dT%H:%M:%S')
+        self.app_name = app_name
+
+    def _get_request_id(self):
+        if not has_app_context():
+            return 'not-in-request'
+        elif not hasattr(g, 'request_id'):
+            return 'no-request-id'
+        else:
+            return g.request_id
+
+    def _build_record(self, record):
+        record.app_name = self.app_name
+        record.request_id = self._get_request_id()
+
+        return record
+
+    def format(self, record):
+        return super(CustomFormatter, self).format(self._build_record(record))
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+import re
+
+from flask._compat import string_types
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -6,15 +9,25 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     ALLOW_EXPLORER = True
     AUTH_REQUIRED = True
+
+    # Logging
+    DM_LOG_LEVEL = 'DEBUG'
+    DM_APP_NAME = 'api'
+    DM_LOG_PATH = '/var/log/digitalmarketplace/application.log'
+    DM_REQUEST_ID_HEADER = 'DM-Request-ID'
+    DM_DOWNSTREAM_REQUEST_ID_HEADER = 'X-Amz-Cf-Id'
+
     DM_API_SERVICES_PAGE_SIZE = 100
     DM_API_SUPPLIERS_PAGE_SIZE = 100
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/digitalmarketplace'
 
-    @staticmethod
-    def init_app(app):
-        pass
+    @classmethod
+    def init_app(cls, app):
+        for name in config_attrs(cls):
+            if name in os.environ:
+                app.config[name] = convert_to_boolean(os.environ[name])
 
 
 class Test(Config):
@@ -38,3 +51,33 @@ config = {
     'production': Live,
     'test': Test,
 }
+
+
+def config_attrs(config):
+    """Returns config attributes from a Config object"""
+    p = re.compile('^[A-Z_]+$')
+    return filter(lambda attr: bool(p.match(attr)), dir(config))
+
+
+def convert_to_boolean(value):
+    """Turn strings to bools if they look like them
+
+    Truthy things should be True
+    >>> for truthy in ['true', 'on', 'yes', '1']:
+    ...   assert convert_to_boolean(truthy) == True
+
+    Falsey things should be False
+    >>> for falsey in ['false', 'off', 'no', '0']:
+    ...   assert convert_to_boolean(falsey) == False
+
+    Other things should be unchanged
+    >>> for value in ['falsey', 'other', True, 0]:
+    ...   assert convert_to_boolean(value) == value
+    """
+    if isinstance(value, string_types):
+        if value.lower() in ['t', 'true', 'on', 'yes', '1']:
+            return True
+        elif value.lower() in ['f', 'false', 'off', 'no', '0']:
+            return False
+
+    return value
