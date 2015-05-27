cb405608678d93b90fc4275e79f70da5c51a0b05
Alexey Bezhan <al@fueledbylemons.com>
2015-05-27 14:42:44 +0100

    Drop 'links' field from incoming supplier data

--- app/main/views/suppliers.py
+++ app/main/views/suppliers.py
@@ -67,6 +67,7 @@ def import_supplier(supplier_id):
     )
 
     supplier_data = supplier_data['suppliers']
+    supplier_data = drop_foreign_fields(supplier_data, ['links'])
 
     json_has_required_keys(
         supplier_data,
