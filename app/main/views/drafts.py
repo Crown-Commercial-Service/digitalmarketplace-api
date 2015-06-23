a9b56624355d48722082fd17e87492e6120b55b6
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-06-23 14:02:14 +0100

    Minor change to improve readability

--- app/main/views/drafts.py
+++ app/main/views/drafts.py
@@ -29,10 +29,12 @@ def create_draft_service(service_id):
         Service.service_id == service_id
     ).first_or_404()
 
-    if DraftService.query.filter(
+    draft_service = DraftService.query.filter(
         DraftService.service_id == service_id
-    ).first():
+    ).first()
+    if draft_service:
         abort(400, "Draft already exists for service {}".format(service_id))
+
     draft = DraftService.from_service(service)
     audit = AuditEvent(
         audit_type=AuditTypes.create_draft_service,
