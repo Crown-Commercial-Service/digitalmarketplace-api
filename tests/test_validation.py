a5c993b82354045ab52190acbe37d6cc00995eb6
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-06-25 16:54:35 +0100

    Add a test for word limit regex validation

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -308,6 +308,15 @@ def test_invalid_url_field_has_validation_error():
     assert "'not_a_url' is not" in errs['serviceDefinitionDocumentURL']
 
 
+def test_too_many_words_causes_validation_error():
+    data = load_example_listing("G7-SCS")
+    data.update({'serviceBenefits': ['more than ten words 5 6 7 8 9 10 11']})
+    errs = get_validation_errors("services-g-cloud-7-scs", data)
+    print("{}".format(errs['serviceBenefits']))
+    assert "'more than ten words 5 6 7 8 9 10 11' does not match" \
+           in errs['serviceBenefits']
+
+
 def assert_example(name, result, expected):
     assert_equal(result, expected)
 
