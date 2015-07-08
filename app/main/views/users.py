2c6513bcafc6901bc6a2f14a3f3b40345c0f7134
Paul Craig <paul.craig@digital.cabinet-office.gov.uk>
2015-07-08 17:04:45 +0100

    Add route to list users.
    Added a route to list users, optionally filtered by `supplier_id`s.
    Kept the logic behind the existing `get_user_by_email` route, but
    changed the url so that it doesn't conflict with the list user method.
    Seems horrible, but.

--- app/main/views/users.py
+++ app/main/views/users.py
@@ -5,11 +5,9 @@ from flask import jsonify, abort, request, current_app
 
 from .. import main
 from ... import db, encryption
-from ...models import User, AuditEvent
-from ...utils import get_json_from_request, json_has_required_keys, \
-    json_has_matching_id
-from ...validation import validate_user_json_or_400, \
-    validate_user_auth_json_or_400
+from ...models import User, AuditEvent, Supplier
+from ...utils import get_json_from_request, json_has_required_keys, json_has_matching_id, pagination_links
+from ...validation import validate_user_json_or_400, validate_user_auth_json_or_400
 
 
 @main.route('/users/auth', methods=['POST'])
@@ -47,6 +45,47 @@ def get_user_by_id(user_id):
 
 
 @main.route('/users', methods=['GET'])
+def list_users():
+    try:
+        page = int(request.args.get('page', 1))
+    except ValueError:
+        abort(400, "Invalid page argument")
+
+    supplier_id = request.args.get('supplier_id')
+
+    if supplier_id is not None:
+        try:
+            supplier_id = int(supplier_id)
+        except ValueError:
+            abort(400, "Invalid supplier_id: %s" % supplier_id)
+
+        supplier = Supplier.query.filter(Supplier.supplier_id == supplier_id).all()
+        if not supplier:
+            abort(404, "supplier_id '%d' not found" % supplier_id)
+
+        users = User.query.filter(User.supplier_id == supplier_id).paginate(
+            page=page,
+            per_page=current_app.config['DM_API_SERVICES_PAGE_SIZE'],
+        )
+
+    # No query parameters, so list all users
+    else:
+        users = User.query.paginate(
+            page=page,
+            per_page=current_app.config['DM_API_SERVICES_PAGE_SIZE'],
+        )
+
+    return jsonify(
+        users=[user.serialize() for user in users.items],
+        links=pagination_links(
+            users,
+            '.list_users',
+            request.args
+        )
+    )
+
+
+@main.route('/users/email-address', methods=['GET'])
 def get_user_by_email():
     email_address = request.args.get('email')
     if email_address is None:
