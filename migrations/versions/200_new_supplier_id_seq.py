e2ef28b6827aed4edec8ebb4b0b78ffff20c7029
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-08-10 12:20:59 +0100

    Renamed the companies house id migrations file
    - file created as companies_house_id and shipped
    - renamed on a different branch leading to issues
    - renamed back here and a new file added rename column

--- migrations/versions/200_new_supplier_id_seq.py
+++ migrations/versions/200_new_supplier_id_seq.py
@@ -1,14 +1,14 @@
 """empty message
 
-Revision ID: 245ef91a1c3a
-Revises: 190_add_companies_house_number
+Revision ID: 200_new_supplier_id_seq
+Revises: 190_add_companies_house_id
 Create Date: 2015-08-06 09:46:13.467484
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '200_new_supplier_id_seq'
-down_revision = '190_add_companies_house_number'
+down_revision = '190_add_companies_house_id'
 
 from alembic import op
 import sqlalchemy as sa
