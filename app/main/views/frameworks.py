83eec9459d3393e0bb7e8dae6cad2872859ccaa2
Chris Hill-Scott <me@quis.cc>
2015-08-17 19:36:12 +0100

    Return some extra numbers
    Including:
    - services broken down by lot
    - suppliers interested in being on the framework

--- app/main/views/frameworks.py
+++ app/main/views/frameworks.py
@@ -1,8 +1,10 @@
 from flask import jsonify
+from sqlalchemy.types import String
+from sqlalchemy import func
 import datetime
 
 from .. import main
-from ...models import Framework, DraftService, Service, User, Supplier, SelectionAnswers
+from ...models import db, Framework, DraftService, Service, User, Supplier, SelectionAnswers, AuditEvent
 
 
 @main.route('/frameworks', methods=['GET'])
@@ -18,12 +20,21 @@ def list_frameworks():
 def get_framework_stats():
 
     seven_days_ago = datetime.datetime.now() + datetime.timedelta(-7)
+    lot_column = DraftService.data['lot'].cast(String).label('lot')
 
     return str({
-        'drafts': DraftService.query.filter(DraftService.status == "not-submitted").count(),
-        'complete': DraftService.query.filter(DraftService.status == "submitted").count(),
+        'services_drafts': DraftService.query.filter(
+                DraftService.status == "not-submitted"
+            ).count(),
+        'services_complete': DraftService.query.filter(
+                DraftService.status == "submitted"
+            ).count(),
+        'services_by_lot': dict(db.session.query(
+                lot_column, func.count(lot_column)
+            ).group_by(lot_column).all()),
         'users': User.query.count(),
         'active_users': User.query.filter(User.logged_in_at > seven_days_ago).count(),
         'suppliers': Supplier.query.count(),
+        'suppliers_interested': AuditEvent.query.filter(AuditEvent.type == 'register_framework_interest').count(),
         'suppliers_with_complete_declaration': SelectionAnswers.find_by_framework('g-cloud-7').count()
     })
