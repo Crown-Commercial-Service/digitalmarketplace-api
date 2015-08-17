9eb73b3ba7b13ea1b0b877febf0c3f862f048104
Chris Hill-Scott <me@quis.cc>
2015-08-17 20:05:41 +0100

    Make queries framework-agnostic

--- app/main/views/frameworks.py
+++ app/main/views/frameworks.py
@@ -16,8 +16,15 @@ def list_frameworks():
     )
 
 
-@main.route('/frameworks/g-cloud-7/stats', methods=['GET'])
-def get_framework_stats():
+@main.route('/frameworks/<string:framework_slug>/stats', methods=['GET'])
+def get_framework_stats(framework_slug):
+
+    framework = Framework.query.filter(
+        Framework.slug == framework_slug
+    ).first()
+
+    if not framework:
+        abort(404, "'{}' is not a framework".format(framework.slug))
 
     seven_days_ago = datetime.datetime.utcnow() + datetime.timedelta(-7)
     lot_column = DraftService.data['lot'].cast(String).label('lot')
@@ -25,13 +32,24 @@ def get_framework_stats():
     return str({
         'services_by_status': dict(db.session.query(
                 DraftService.status, func.count(DraftService.status)
-            ).group_by(DraftService.status)),
+            ).group_by(
+                DraftService.status
+            ).filter(
+                DraftService.framework_id == framework.id
+            )),
         'services_by_lot': dict(db.session.query(
                 lot_column, func.count(lot_column)
-            ).group_by(lot_column).all()),
+            ).group_by(
+                lot_column
+            ).filter(
+                DraftService.framework_id == framework.id
+            ).all()),
         'users': User.query.count(),
         'active_users': User.query.filter(User.logged_in_at > seven_days_ago).count(),
         'suppliers': Supplier.query.count(),
-        'suppliers_interested': AuditEvent.query.filter(AuditEvent.type == 'register_framework_interest').count(),
-        'suppliers_with_complete_declaration': SelectionAnswers.find_by_framework('g-cloud-7').count()
+        'suppliers_interested': AuditEvent.query.filter(
+                AuditEvent.data['frameworkSlug'].cast(String) == framework_slug,
+                AuditEvent.type == 'register_framework_interest'
+            ).count(),
+        'suppliers_with_complete_declaration': SelectionAnswers.find_by_framework(framework_slug).count()
     })
