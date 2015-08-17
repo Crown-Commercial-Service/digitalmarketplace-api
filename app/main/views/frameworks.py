009a7f67d1aa67b58a558b769474b2ffc104fc26
Chris Hill-Scott <me@quis.cc>
2015-08-17 16:44:23 +0100

    Add an endpoint for stats about a framework
    This commit:
    - gives us count for some things that we think we might want to count,
      based on: https://www.pivotaltracker.com/story/show/100215966
    
    It doesn't:
    - Deal with snapshotting these counts over time, which will probably happen in
      a separate app

--- app/main/views/frameworks.py
+++ app/main/views/frameworks.py
@@ -1,7 +1,8 @@
 from flask import jsonify
+import datetime
 
 from .. import main
-from ...models import Framework
+from ...models import Framework, DraftService, Service, User, Supplier, SelectionAnswers
 
 
 @main.route('/frameworks', methods=['GET'])
@@ -11,3 +12,18 @@ def list_frameworks():
     return jsonify(
         frameworks=[f.serialize() for f in frameworks]
     )
+
+
+@main.route('/frameworks/g-cloud-7/stats', methods=['GET'])
+def get_framework_stats():
+
+    seven_days_ago = datetime.datetime.now() + datetime.timedelta(-7)
+
+    return str({
+        'drafts': DraftService.query.filter(DraftService.status == "not-submitted").count(),
+        'complete': DraftService.query.filter(DraftService.status == "submitted").count(),
+        'users': User.query.count(),
+        'active_users': User.query.filter(User.logged_in_at > seven_days_ago).count(),
+        'suppliers': Supplier.query.count(),
+        'suppliers_with_complete_declaration': SelectionAnswers.find_by_framework('g-cloud-7').count()
+    })
