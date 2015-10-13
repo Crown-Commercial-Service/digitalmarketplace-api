82ebdfc34f64bf2761b5f8575db38814b69ba7c6
Alexey Bezhan <al@fueledbylemons.com>
2015-10-13 16:00:37 +0100

    Strip whitespace from status and framework /services params

--- app/main/views/services.py
+++ app/main/views/services.py
@@ -42,12 +42,16 @@ def list_services():
     supplier_id = request.args.get('supplier_id')
 
     if request.args.get('framework'):
-        services = Service.query.has_frameworks(*request.args['framework'].split(','))
+        services = Service.query.has_frameworks(*[
+            slug.strip() for slug in request.args['framework'].split(',')
+        ])
     else:
         services = Service.query.framework_is_live()
 
     if request.args.get('status'):
-        services = services.has_statuses(*request.args['status'].split(','))
+        services = services.has_statuses(*[
+            status.strip() for status in request.args['status'].split(',')
+        ])
 
     if supplier_id is not None:
         try:
