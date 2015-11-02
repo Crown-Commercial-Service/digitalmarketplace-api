593e826b24d83997a5be450be1401e16ec17c07c
Alexey Bezhan <al@fueledbylemons.com>
2015-11-02 17:33:00 +0000

    Use new dmutils init_manager to set up reload on schema changes

--- application.py
+++ application.py
@@ -4,24 +4,18 @@ from __future__ import print_function
 
 import os
 
-from flask.ext.script import Manager, Server
+from dmutils import init_manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
 from app import create_app, db
 
 
 application = create_app(os.getenv('DM_ENVIRONMENT') or 'development')
-manager = Manager(application)
-manager.add_command("runserver", Server(port=5000))
+manager = init_manager(application, 5000, ['./json_schemas'])
+
 migrate = Migrate(application, db)
 manager.add_command('db', MigrateCommand)
 
-
-@manager.command
-def list_routes():
-    """List URLs of all application routes."""
-    for rule in sorted(application.url_map.iter_rules(), key=lambda r: r.rule):
-        print("{:10} {}".format(", ".join(rule.methods - set(['OPTIONS', 'HEAD'])), rule.rule))
 
 if __name__ == '__main__':
     manager.run()
