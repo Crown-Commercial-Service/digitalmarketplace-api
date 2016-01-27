de9135b6d8f532cde101ca295bdcc6de7336c9f2
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2016-01-27 10:28:09 +0000

    Add basic create brief route

---
+++ app/main/views/briefs.py
@@ -0,0 +1,30 @@
+from flask import jsonify, abort
+
+from .. import main
+from ... import db
+from ...utils import get_json_from_request, json_has_required_keys
+from ...service_utils import validate_and_return_lot
+from ...models import User, Brief
+
+
+@main.route('/briefs', methods=['POST'])
+def create_a_brief():
+    json_payload = get_json_from_request()
+    json_has_required_keys(json_payload, ['briefs'])
+    json_payload = json_payload['briefs']
+
+    json_has_required_keys(json_payload, ['frameworkSlug', 'lot', 'userId'])
+
+    framework, lot = validate_and_return_lot(json_payload)
+
+    user = User.query.get(json_payload.pop('userId'))
+
+    if user is None:
+        abort(400, "User ID does not exist")
+
+    brief = Brief(data={}, users=[user], framework=framework, lot=lot)
+
+    db.session.add(brief)
+    db.session.commit()
+
+    return jsonify(), 201
--- tests/app/views/test_briefs.py
+++ tests/app/views/test_briefs.py
@@ -1,3 +1,5 @@
+import json
+
 from ..helpers import BaseApplicationTest
 
 from app import db
@@ -6,11 +8,36 @@ from app import db
 class TestBriefs(BaseApplicationTest):
     def setup(self):
         super(TestBriefs, self).setup()
-        self.setup_dummy_user()
-        with self.app.app_context():
-            db.session.commit()
+        self.user_id = self.setup_dummy_user(role='buyer')
 
     def test_create_brief_with_no_data(self):
-        res = self.client.post('/briefs')
+        res = self.client.post(
+            '/briefs',
+            content_type='application/json')
 
         assert res.status_code == 400
+
+    def test_create_brief(self):
+        res = self.client.post(
+            '/briefs',
+            data=json.dumps({'briefs': {
+                'userId': self.user_id,
+                'frameworkSlug': 'g-cloud-7',
+                'lot': 'iaas',
+            }}),
+            content_type='application/json')
+
+        assert res.status_code == 201
+
+    def test_create_brief_fails_if_user_does_not_exist(self):
+        res = self.client.post(
+            '/briefs',
+            data=json.dumps({'briefs': {
+                'userId': 999,
+                'frameworkSlug': 'g-cloud-7',
+                'lot': 'iaas',
+            }}),
+            content_type='application/json')
+
+        assert res.status_code == 400
+        assert json.loads(res.get_data(as_text=True))['error'] == 'User ID does not exist'
