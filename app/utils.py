e9c4065c96148a0e46e543a693aeba6b590d3c78
Alexey Bezhan <al@fueledbylemons.com>
2016-02-10 19:13:47 +0000

    Check item types when removing whitespace for data arrays
    Essential/non-essential brief question answers are lists of booleans
    so whitespace utility function has to check for item types.

--- app/utils.py
+++ app/utils.py
@@ -98,7 +98,7 @@ def strip_whitespace_from_data(data):
         if isinstance(value, list):
             # Strip whitespace and remove empty items from lists
             data[key] = list(
-                filter(None, map((lambda x: x.strip()), value))
+                filter(None, map(lambda x: x.strip() if isinstance(x, string_types) else x, value))
             )
         elif isinstance(value, string_types):
             # Strip whitespace from strings
