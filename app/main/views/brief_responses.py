bd2c405e4b806c80f2166a8be8a34adaf1e8ca78
Alexey Bezhan <al@fueledbylemons.com>
2016-02-10 13:30:51 +0000

    Add create brief response endpoint

---
+++ app/main/views/brief_responses.py
@@ -0,0 +1,59 @@
+from flask import jsonify, abort
+from sqlalchemy.exc import IntegrityError, DataError
+
+from .. import main
+from ...models import db, Supplier, Brief, BriefResponse
+from ...utils import (
+    get_json_from_request, json_has_required_keys,
+    validate_and_return_updater_request,
+)
+
+from ...service_utils import validate_and_return_supplier
+
+
+@main.route('/brief-responses', methods=['POST'])
+def create_brief_response():
+    json_payload = get_json_from_request()
+    updater_json = validate_and_return_updater_request()
+
+    json_has_required_keys(json_payload, ['briefResponses'])
+    brief_response_json = json_payload['briefResponses']
+    json_has_required_keys(brief_response_json, ['briefId', 'supplierId'])
+
+    try:
+        brief = Brief.query.get(brief_response_json['briefId'])
+    except DataError:
+        brief = None
+
+    if brief is None:
+        abort(400, "Invalid brief ID '{}'".format(brief_response_json['briefId']))
+
+    if brief.status != 'live':
+        abort(400, "Brief must be live")
+
+    if brief.framework.status != 'live':
+        abort(400, "Brief framework must be live")
+
+    supplier = validate_and_return_supplier(brief_response_json)
+
+    brief_response = BriefResponse(
+        data=brief_response_json,
+        supplier=supplier,
+        brief=brief,
+    )
+
+    db.session.add(brief_response)
+    try:
+        db.session.flush()
+    except IntegrityError as e:
+        db.session.rollback()
+        abort(400, e.orig)
+
+    db.session.commit()
+
+    return jsonify(briefResponses=brief_response.serialize()), 201
+
+
+@main.route('/brief-responses/<int:brief_response_id>', methods=['POST'])
+def get_brief_response(brief_response_id):
+    pass
--- tests/app/views/test_brief_response.py
+++ tests/app/views/test_brief_response.py
@@ -1,6 +1,155 @@
+import json
+
 from ..helpers import BaseApplicationTest, JSONUpdateTestMixin
+
+from app.models import db, Lot, Brief
 
 
 class TestCreateBriefResponse(BaseApplicationTest, JSONUpdateTestMixin):
     endpoint = '/brief-responses'
     method = 'post'
+
+    def setup(self):
+        super(TestCreateBriefResponse, self).setup()
+
+        with self.app.app_context():
+            self.setup_dummy_suppliers(1)
+            brief = Brief(
+                data={}, status='live', framework_id=5, lot=Lot.query.get(5)
+            )
+            db.session.add(brief)
+            db.session.commit()
+
+            self.brief_id = brief.id
+
+    def create_brief_response(self, data):
+        return self.client.post(
+            '/brief-responses',
+            data=json.dumps({
+                'updated_by': 'test@example.com',
+                'briefResponses': data,
+            }),
+            content_type='application/json'
+        )
+
+    def test_create_new_brief_response(self, live_framework):
+        res = self.create_brief_response({
+            'briefId': self.brief_id,
+            'supplierId': 0,
+        })
+
+        data = json.loads(res.get_data(as_text=True))
+
+        assert res.status_code == 201
+        assert data['briefResponses']['supplierName'] == 'Supplier 0'
+        assert data['briefResponses']['briefId'] == self.brief_id
+
+    def test_cannot_create_brief_response_with_empty_json(self, live_framework):
+        res = self.client.post(
+            '/brief-responses',
+            data=json.dumps({
+                'updated_by': 'test@example.com',
+            }),
+            content_type='application/json'
+        )
+
+        assert res.status_code == 400
+
+    def test_cannot_create_brief_response_with_invalid_json(self, live_framework):
+        res = self.client.post(
+            '/brief-responses',
+            data=json.dumps({
+                'updated_by': 'test@example.com',
+                'briefResponses': {
+                    'briefId': self.brief_id,
+                    'supplierId': 0,
+                    'niceToHaveRequirements': 10
+                }
+            }),
+            content_type='application/json'
+        )
+
+        data = json.loads(res.get_data(as_text=True))
+
+        assert res.status_code == 400
+        assert data['error']['essentialRequirements'] == 'answer_required'
+        assert 'niceToHaveRequirements' in data['error']
+
+    def test_cannot_create_brief_response_without_supplier_id(self, live_framework):
+        res = self.create_brief_response({
+            'briefId': self.brief_id
+        })
+
+        assert res.status_code == 400
+        assert 'supplierId' in res.get_data(as_text=True)
+
+    def test_cannot_create_brief_response_without_brief_id(self, live_framework):
+        res = self.create_brief_response({
+            'supplierId': 0
+        })
+
+        assert res.status_code == 400
+        assert 'briefId' in res.get_data(as_text=True)
+
+    def test_cannot_create_brief_response_with_non_integer_supplier_id(self, live_framework):
+        res = self.create_brief_response({
+            'briefId': self.brief_id,
+            'supplierId': 'not a number',
+        })
+
+        assert res.status_code == 400
+        assert 'Invalid supplier ID' in res.get_data(as_text=True)
+
+    def test_cannot_create_brief_response_with_non_integer_brief_id(self, live_framework):
+        res = self.create_brief_response({
+            'briefId': 'not a number',
+            'supplierId': 0,
+        })
+
+        assert res.status_code == 400
+        assert 'Invalid brief ID' in res.get_data(as_text=True)
+
+    def test_cannot_create_brief_response_when_brief_doesnt_exist(self, live_framework):
+        res = self.create_brief_response({
+            'briefId': self.brief_id + 1,
+            'supplierId': 0
+        })
+
+        assert res.status_code == 400
+        assert 'Invalid brief ID' in res.get_data(as_text=True)
+
+    def test_cannot_create_brief_response_when_supplier_doesnt_exist(self, live_framework):
+        res = self.create_brief_response({
+            'briefId': self.brief_id,
+            'supplierId': 999
+        })
+
+        assert res.status_code == 400
+        assert 'Invalid supplier ID' in res.get_data(as_text=True)
+
+    def test_cannot_respond_to_a_brief_that_isnt_live(self, live_framework):
+        with self.app.app_context():
+            brief = Brief(
+                data={}, status='draft', framework_id=5, lot=Lot.query.get(5)
+            )
+            db.session.add(brief)
+            db.session.commit()
+
+            brief_id = brief.id
+
+        res = self.create_brief_response({
+            'briefId': brief_id,
+            'supplierId': 0
+        })
+
+        assert res.status_code == 400
+        assert "Brief must be live" in res.get_data(as_text=True)
+
+    def test_cannot_respond_to_an_expired_framework_brief(self, expired_framework):
+        res = self.create_brief_response({
+            'briefId': self.brief_id,
+            'supplierId': 0
+        })
+
+        assert res.status_code == 400
+        assert "Brief framework must be live" in res.get_data(as_text=True)
