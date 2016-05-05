acd5a2bbd43add0561b36a529c537b64f8d34956
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-05-05 14:49:50 +0100

    Migration to add `data` to `lots` table with info about units
    This is a JSON blob that will store additional data about the lot that is needed by the frontend applications but that we will never need to index or search by (so it doesn't need a new column for each item).
    
    This also adds the "singular" and "plural" units for each lot - i.e. service/supplier/lab.

---
+++ migrations/versions/600_add_units_to_lots.py
@@ -0,0 +1,38 @@
+"""Add units to lots, for display by the frontend aps
+
+Revision ID: 600
+Revises: 590
+Create Date: 2016-05-05 10:49:45.258601
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '600'
+down_revision = '590'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+def upgrade():
+    op.add_column('lots', sa.Column('data', postgresql.JSON(), nullable=True))
+    op.execute("""
+        UPDATE lots
+          SET data = '{"unitSingular": "service", "unitPlural": "services"}'
+          WHERE  slug in ('saas', 'paas', 'iaas', 'scs', 'digital-outcomes', 'user-research-participants');
+    """)
+    op.execute("""
+        UPDATE lots
+          SET data = '{"unitSingular": "specialist", "unitPlural": "specialists"}'
+          WHERE  slug = 'digital-specialists';
+    """)
+    op.execute("""
+        UPDATE lots
+          SET data = '{"unitSingular": "lab", "unitPlural": "labs"}'
+          WHERE  slug = 'user-research-studios';
+    """)
+
+
+def downgrade():
+    op.drop_column('lots', 'data')
