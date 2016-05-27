488de4123edc0212dfb71d902de938f043fa3c28
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2016-05-27 12:26:03 +0100

    add scripts/generate_model_diagram.py
    A very basic, all-defaults invocation of sadisplay on models.py, emitting a dot file

---
+++ scripts/generate_model_diagram.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from sys import stdout
+
+import sadisplay
+from app import create_app
+from app import models
+
+
+def _build_desc():
+    return sadisplay.describe(
+        [getattr(models, attr) for attr in dir(models)],
+        show_methods=True,
+        show_properties=True,
+        show_indexes=True,
+    )
+
+if __name__ == "__main__":
+    app = create_app("development")
+    with app.app_context():
+        desc = _build_desc()
+        stdout.write(sadisplay.dot(desc).encode("utf8"))
