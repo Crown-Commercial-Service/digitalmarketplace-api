79e77d9313f0a3098054e4d453d9b2b25b746616
sarneaud <sarneaud@users.noreply.github.com>
2016-07-26 15:23:19 +1000

    Remove API explorer
    This wasn't being maintained by the UK devs, either, anyway.

--- app/explorer/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# These modules depend on explorer already being set up.  PEP8 rule disabled.
-# FIXME: refactor out circular import
-explorer = Blueprint('explorer', __name__)  # noqa
-
-from . import views
--- app/explorer/views.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-
-from . import explorer
-
-
-@explorer.route('/_explorer')
-def explorer():
-    return render_template('explorer.html')
