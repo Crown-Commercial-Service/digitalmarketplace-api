9a7a6314ad8ce98e530f4934b5985798b64f604b
matt-sm <mattysmith@gmail.com>
2016-07-26 11:56:42 +1000

    remove details

--- scripts/ci_deploy.sh
+++ scripts/ci_deploy.sh
@@ -8,7 +8,7 @@ set -x
 curl -v -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
 sudo dpkg -i cf-cli_amd64.deb
 cf -v
-cf login -a https://api.system.staging.digital.gov.au -o dto -u matt.smith@digital.gov.au -p matt!QAZ
+cf login -a https://api.system.staging.digital.gov.au -o dto -u $CF_USER_STAGING -p $CF_PASSWORD_STAGING
 cf target -o dto -s digital-marketplace
 cf push -c 'python application.py db upgrade'
 cf push
