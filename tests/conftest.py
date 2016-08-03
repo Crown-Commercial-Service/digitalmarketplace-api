9bbd758a5cfdfacb95a82ee8d722215c30df046c
Paul Craig <paul_craig_16@hotmail.com>
2016-08-03 17:12:07 +0100

    Return a function from a fixture
    This is kind of a hack that lets us define an inner
    function that accepts parameters inside of our fixture.
    This way, we can directly call the fixure in our test and pass in
    a couple of parameters.
    It's totally weird because you're not meant to be calling fixtures
    directly inside the test, so it's taking advantage of
    incidental behaviour.
    
    [Based on this stackoverflow answer](http://stackoverflow.com/questions/18011902/py-test-pass-a-parameter-to-a-fixture-function).

--- tests/conftest.py
+++ tests/conftest.py
@@ -51,3 +51,32 @@ def live_framework(request, app):
 @pytest.fixture(params=[('expired', 'digital-outcomes-and-specialists')])
 def expired_framework(request, app):
     _update_framework(request, app, request.param[0], request.param[1])
+
+
+@pytest.fixture()
+def update_framework(request, app):
+    def inner_update_framework(status='live', framework_slug='digital-outcomes-and-specialists'):
+        with app.app_context():
+            framework = Framework.query.filter(
+                Framework.slug == framework_slug
+            ).first()
+            original_framework_status = framework.status
+            framework.status = status
+
+            db.session.add(framework)
+            db.session.commit()
+
+        def teardown():
+            with app.app_context():
+                framework = Framework.query.filter(
+                    Framework.slug == framework_slug
+                ).first()
+                framework.status = original_framework_status
+
+                db.session.add(framework)
+                db.session.commit()
+
+        request.addfinalizer(teardown)
+
+    return inner_update_framework
+
