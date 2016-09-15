78b52a9796fe3c328c6b0177f3556e969c46f6b4
prakash <prakash@timilsina.com>
2016-09-15 15:26:21 +1000

    splitted dashboard endpoints

--- app/main/views/dashboard.py
+++ app/main/views/dashboard.py
@@ -8,30 +8,26 @@ from .. import main
 from ...models import (Brief, Supplier, ServiceRole, PriceSchedule, User)
 
 
-@main.route('/dashboard/stats', methods=['GET'])
-def get_dashboard_stats():
-    # workaround as 'Brief.withdrawn_at == None' gives pep8 error
-    brief_query = Brief.query.filter(Brief.withdrawn_at.is_(None), Brief.published_at.isnot(None))
-    all_briefs = brief_query.order_by(desc(Brief.published_at)).all()
-
-    briefs = {
-        'open_to_all': brief_query.filter(Brief.data['sellerSelector'].astext == 'allSellers').count(),
-
-        'open_to_selected': brief_query.filter(Brief.data['sellerSelector'].astext == 'someSellers').count(),
-
-        'open_to_one': brief_query.filter(Brief.data['sellerSelector'].astext == 'oneSellers').count(),
-
-        'recent_brief_time_since': (timesince(all_briefs[0].published_at)) if all_briefs else ''
-    }
-
-    suppliers = {
-        "total": Supplier.query.count()
-    }
-
+@main.route('/dashboard/buyers', methods=['GET'])
+def get_buyers_stats():
     buyers = {
         'total': User.query.filter(User.active.is_(True), User.role == 'buyer').count()
     }
 
+    return jsonify(buyers=buyers)
+
+
+@main.route('/dashboard/suppliers', methods=['GET'])
+def get_suppliers_stats():
+    suppliers = {
+        "total": Supplier.query.count()
+    }
+
+    return jsonify(suppliers=suppliers)
+
+
+@main.route('/dashboard/roles', methods=['GET'])
+def get_roles_stats():
     top_roles = []
     all_roles = ServiceRole.query.all()
 
@@ -55,11 +51,23 @@ def get_dashboard_stats():
         'top_roles': sorted(top_roles, key=itemgetter('count'), reverse=True)
     }
 
-    stats = {
-        'briefs': briefs,
-        'suppliers': suppliers,
-        'buyers': buyers,
-        'roles': roles,
+    return jsonify(roles=roles)
+
+
+@main.route('/dashboard/briefs', methods=['GET'])
+def get_briefs_stats():
+    # workaround as 'Brief.withdrawn_at == None' gives pep8 error
+    brief_query = Brief.query.filter(Brief.withdrawn_at.is_(None), Brief.published_at.isnot(None))
+    all_briefs = brief_query.order_by(desc(Brief.published_at)).all()
+
+    briefs = {
+        'open_to_all': brief_query.filter(Brief.data['sellerSelector'].astext == 'allSellers').count(),
+
+        'open_to_selected': brief_query.filter(Brief.data['sellerSelector'].astext == 'someSellers').count(),
+
+        'open_to_one': brief_query.filter(Brief.data['sellerSelector'].astext == 'oneSellers').count(),
+
+        'recent_brief_time_since': (timesince(all_briefs[0].published_at)) if all_briefs else ''
     }
 
-    return jsonify(stats=stats)
+    return jsonify(briefs=briefs)
--- tests/app/views/test_dashboard.py
+++ tests/app/views/test_dashboard.py
@@ -4,16 +4,26 @@ from ..helpers import BaseApplicationTest
 
 
 class TestDashboard(BaseApplicationTest):
-    def stats(self):
-        return self.client.get('/dashboard/stats')
 
-    def test_get_stats(self):
-        response = self.stats()
+    def test_get_buyers(self):
+        response = self.client.get('/dashboard/buyers')
+        assert response.status_code == 200
+
+    def test_get_suppliers(self):
+        response = self.client.get('/dashboard/suppliers')
+        assert response.status_code == 200
+
+    def test_get_briefs(self):
+        response = self.client.get('/dashboard/briefs')
+        assert response.status_code == 200
+
+    def test_get_roles(self):
+        response = self.client.get('/dashboard/roles')
         assert response.status_code == 200
 
         data = json.loads(response.get_data())
 
         # Check if 'junior' and 'senior' have been stripped from the name
-        for role in data['stats']['roles']['top_roles']:
+        for role in data['roles']['top_roles']:
             assert 'junior' not in role['name'].lower()
             assert 'senior' not in role['name'].lower()
