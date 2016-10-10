95d83bddef8432409b1100fbb0b0867bdb103429
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-10-10 16:18:46 +0100

    `SupplierFramework.current_framework_agreement` picks relevant FA
    `SupplierFramework.current_framework_agreement` now supports
    multiple framework agreements per supplier framework.
    
    For the moment, we include drafts in the SupplierFramework to keep our interface the same whilst we refactor
    the frontend to allow multiple framework agreements per supplier framework. This means that if a draft is created
    after any other framework agreement then it must take precident (as the supplier frontend for the moment only
    knows about one framework agreement (the current framework agreement) we must expose the draft so it can be
    edited as they complete the signing flow).
    
    Note, as we have no timestamp for framework agreements being created we instead have to use the highest `id`.
    To support this we want to restrict the order in which our framework agreements are returned by ordering the
    backref by `FrameworkAgreement.id` instead of the default SQLAlchemy order of most recently updated.
    
    Also note, this means that for a short while, if someone has
    signed/countersigned a supplier framework and then starts a new draft then the supplier framework will return to
    'draft' status and will lose knowledge of the previous signing.
    
    After the supplier frontend has been refactored so that the signing flow deals with a framework agreement rather
    than a suqpplier framework then we will be able to ignore drafts and not include them in the current framework
     agreement

--- tests/app/test_model.py
+++ tests/app/test_model.py
@@ -878,7 +878,7 @@ class TestFrameworkAgreements(BaseApplicationTest):
         )
         assert framework_agreement.status == 'on hold'
 
-    def test_countersigned_framework_agreement_status_is_approved(self):
+    def test_approved_framework_agreement_status_is_approved(self):
         framework_agreement = FrameworkAgreement(
             supplier_id=0,
             framework_id=1,
@@ -900,3 +900,119 @@ class TestFrameworkAgreements(BaseApplicationTest):
             countersigned_agreement_path='/path/to/the/countersignedAgreement.pdf'
         )
         assert framework_agreement.status == 'countersigned'
+
+
+class TestCurrentFrameworkAgreement(BaseApplicationTest):
+    """
+    Tests the current_framework_agreement property of SupplierFramework objects
+    """
+    BASE_AGREEMENT_KWARGS = {
+        "supplier_id": 0,
+        "framework_id": 1,
+        "signed_agreement_details": {"agreement": "details"},
+        "signed_agreement_path": "path",
+    }
+
+    def setup(self):
+        super(TestCurrentFrameworkAgreement, self).setup()
+        with self.app.app_context():
+            self.setup_dummy_suppliers(1)
+
+            supplier_framework = SupplierFramework(supplier_id=0, framework_id=1)
+            db.session.add(supplier_framework)
+            db.session.commit()
+
+    def get_supplier_framework(self):
+        return SupplierFramework.query.filter(
+            SupplierFramework.supplier_id == 0,
+            SupplierFramework.framework_id == 1
+        ).first()
+
+    def test_current_framework_agreement_with_no_agreements(self):
+        with self.app.app_context():
+            supplier_framework = self.get_supplier_framework()
+            assert supplier_framework.current_framework_agreement is None
+
+    def test_current_framework_agreement_with_one_draft_only(self):
+        with self.app.app_context():
+            db.session.add(FrameworkAgreement(id=5, **self.BASE_AGREEMENT_KWARGS))
+            db.session.commit()
+            supplier_framework = self.get_supplier_framework()
+            assert supplier_framework.current_framework_agreement.id == 5
+
+    def test_current_framework_agreement_with_multiple_drafts(self):
+        with self.app.app_context():
+            db.session.add(FrameworkAgreement(id=5, **self.BASE_AGREEMENT_KWARGS))
+            db.session.add(FrameworkAgreement(id=6, **self.BASE_AGREEMENT_KWARGS))
+            db.session.commit()
+            supplier_framework = self.get_supplier_framework()
+            assert supplier_framework.current_framework_agreement.id == 6
+
+    def test_current_framework_agreement_with_one_signed(self):
+        with self.app.app_context():
+            db.session.add(FrameworkAgreement(
+                id=5, signed_agreement_returned_at=datetime(2016, 10, 9, 12, 00, 00), **self.BASE_AGREEMENT_KWARGS)
+            )
+            supplier_framework = self.get_supplier_framework()
+            assert supplier_framework.current_framework_agreement.id == 5
+
+    def test_current_framework_agreement_with_multiple_signed(self):
+        with self.app.app_context():
+            db.session.add(FrameworkAgreement(
+                id=5, signed_agreement_returned_at=datetime(2016, 10, 9, 12, 00, 00), **self.BASE_AGREEMENT_KWARGS)
+            )
+            db.session.add(FrameworkAgreement(
+                id=6, signed_agreement_returned_at=datetime(2016, 10, 10, 12, 00, 00), **self.BASE_AGREEMENT_KWARGS)
+            )
+            supplier_framework = self.get_supplier_framework()
+            assert supplier_framework.current_framework_agreement.id == 6
+
+    def test_current_framework_agreement_with_signed_and_old_draft(self):
+        # THIS IS GOING TO DIE ONCE WE STOP RETURNING DRAFTS IN SUPPLIER FRAMEWORK
+        # RELATIVE AGE IS DETERMINED BY ID ORDER
+        with self.app.app_context():
+            db.session.add(FrameworkAgreement(id=5, **self.BASE_AGREEMENT_KWARGS))
+            db.session.add(FrameworkAgreement(
+                id=6, signed_agreement_returned_at=datetime(2016, 10, 10, 12, 00, 00), **self.BASE_AGREEMENT_KWARGS)
+            )
+            supplier_framework = self.get_supplier_framework()
+            assert supplier_framework.current_framework_agreement.id == 6
+
+    def test_current_framework_agreement_with_signed_and_new_draft(self):
+        # THIS IS GOING TO DIE ONCE WE STOP RETURNING DRAFTS IN SUPPLIER FRAMEWORK
+        # RELATIVE AGE IS DETERMINED BY ID ORDER
+        with self.app.app_context():
+            db.session.add(FrameworkAgreement(id=6, **self.BASE_AGREEMENT_KWARGS))
+            db.session.add(FrameworkAgreement(
+                id=5, signed_agreement_returned_at=datetime(2016, 10, 10, 12, 00, 00), **self.BASE_AGREEMENT_KWARGS)
+            )
+            supplier_framework = self.get_supplier_framework()
+            assert supplier_framework.current_framework_agreement.id == 6
+
+    def test_current_framework_agreement_with_signed_and_new_countersigned(self):
+        with self.app.app_context():
+            db.session.add(FrameworkAgreement(
+                id=5, signed_agreement_returned_at=datetime(2016, 10, 9, 12, 00, 00), **self.BASE_AGREEMENT_KWARGS)
+            )
+            db.session.add(FrameworkAgreement(
+                id=6,
+                signed_agreement_returned_at=datetime(2016, 10, 10, 12, 00, 00),
+                countersigned_agreement_returned_at=datetime(2016, 10, 11, 12, 00, 00),
+                **self.BASE_AGREEMENT_KWARGS)
+            )
+            supplier_framework = self.get_supplier_framework()
+            assert supplier_framework.current_framework_agreement.id == 6
+
+    def test_current_framework_agreement_with_countersigned_and_new_signed(self):
+        with self.app.app_context():
+            db.session.add(FrameworkAgreement(
+                id=5, signed_agreement_returned_at=datetime(2016, 10, 11, 12, 00, 00), **self.BASE_AGREEMENT_KWARGS)
+            )
+            db.session.add(FrameworkAgreement(
+                id=6,
+                signed_agreement_returned_at=datetime(2016, 10, 9, 12, 00, 00),
+                countersigned_agreement_returned_at=datetime(2016, 10, 10, 12, 00, 00),
+                **self.BASE_AGREEMENT_KWARGS)
+            )
+            supplier_framework = self.get_supplier_framework()
+            assert supplier_framework.current_framework_agreement.id == 5
