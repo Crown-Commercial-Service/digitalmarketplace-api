5085224eec36d76e16ced6a1ff53e8d60ccc2503
Daniel Knell <contact@danielknell.co.uk>
2016-11-08 15:46:24 +0000

    remove unused function from bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,19 +7,6 @@
 
 set -o pipefail
 
-function display_result {
-  RESULT=$1
-  EXIT_STATUS=$2
-  TEST=$3
-
-  if [ $RESULT -ne 0 ]; then
-    echo -e "\033[31m$TEST failed\033[0m"
-    exit $EXIT_STATUS
-  else
-    echo -e "\033[32m$TEST passed\033[0m"
-  fi
-}
-
 if [ ! $VIRTUAL_ENV ]; then
   virtualenv ./venv
   . ./venv/bin/activate
