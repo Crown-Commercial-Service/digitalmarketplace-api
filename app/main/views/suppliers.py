69ad8aeb2bd8cece45dccc7a3691d5d4485636aa
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2016-11-17 17:36:46 +0000

    get_supplier_frameworks_info view: minor renaming of a misleading variable name

--- app/main/views/suppliers.py
+++ app/main/views/suppliers.py
@@ -324,12 +324,12 @@ def get_supplier_frameworks_info(supplier_id):
     ).all()
 
     return jsonify(frameworkInterest=[
-        framework.serialize({
-            'drafts_count': service_counts.get((framework.framework_id, 'not-submitted'), 0),
-            'complete_drafts_count': service_counts.get((framework.framework_id, 'submitted'), 0),
-            'services_count': service_counts.get((framework.framework_id, 'published'), 0)
+        supplier_framework.serialize({
+            'drafts_count': service_counts.get((supplier_framework.framework_id, 'not-submitted'), 0),
+            'complete_drafts_count': service_counts.get((supplier_framework.framework_id, 'submitted'), 0),
+            'services_count': service_counts.get((supplier_framework.framework_id, 'published'), 0)
         })
-        for framework in supplier_frameworks]
+        for supplier_framework in supplier_frameworks]
     )
 
 
