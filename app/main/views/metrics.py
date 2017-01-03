278799aa5bf04c54a022dcbf262aeea6d5a4c5c3
Alex Sadleir <alex.sadleir@digital.gov.au>
2017-01-03 15:00:15 +1100

    Add application metrics

--- app/main/views/metrics.py
+++ app/main/views/metrics.py
@@ -1,11 +1,13 @@
 from .. import main
 from . import briefs, users, suppliers
-from ...models import Brief, User, Supplier, BriefResponse
+from ...models import Application, Brief, User, Supplier, BriefResponse
 from ... import db
-from sqlalchemy import desc, func, select
+from sqlalchemy import desc, func, select, and_
 import pendulum
 import json
-from flask import jsonify
+import StringIO
+import csv
+from flask import jsonify, make_response
 
 
 @main.route('/metrics', methods=['GET'])
@@ -29,6 +31,63 @@ def get_metrics():
     return jsonify(metrics)
 
 
+@main.route('/metrics/applications', methods=['GET'])
+def get_application_metrics():
+    timestamp = pendulum.now().to_iso8601_string()
+    metrics = {}
+
+    application_count = Application.query.count()
+    metrics["application_total_count"] = {"value": application_count, "ts": timestamp}
+    application_existing_seller_count = Application.query\
+        .filter(Application.supplier_code.isnot(None)).count()
+    metrics["application_new_seller_count"] = {"value": application_count - application_existing_seller_count,
+                                               "ts": timestamp}
+    metrics["application_existing_seller_count"] = {"value": application_existing_seller_count, "ts": timestamp}
+
+    suppliers_total = Supplier.query.filter(Supplier.abn != Supplier.DUMMY_ABN).count()
+    suppliers_with_apps_count = Supplier.query.filter(Supplier.abn != Supplier.DUMMY_ABN)\
+        .join(Application, and_(Application.supplier_code == Supplier.code)).distinct(Supplier.id).count()
+    metrics["suppliers_with_application_count"] = \
+        {"value": suppliers_with_apps_count, "ts": timestamp}
+    metrics["suppliers_without_application_count"] = \
+        {"value": suppliers_total - suppliers_with_apps_count, "ts": timestamp}
+
+    applications_by_status = select([Application.status,
+                                     func.count(Application.status), func.count(Application.supplier_code)]) \
+        .order_by(Application.status) \
+        .group_by(Application.status)
+    for (status, count, existing_seller) in db.session.execute(applications_by_status):
+        metrics["application_status_{}_total_count".format(status)] = \
+            {"value": count, "ts": timestamp}
+        metrics["application_status_{}_existing_seller_count".format(status)] = \
+            {"value": existing_seller, "ts": timestamp}
+        metrics["application_status_{}_new_seller_count".format(status)] = \
+            {"value": count - existing_seller, "ts": timestamp}
+
+    for status in ['saved', 'submitted', 'approved', 'approval_rejected', 'complete', 'assessment_rejected']:
+        for category in ['existing_seller', 'new_seller', 'total']:
+            if "application_status_{}_{}_count".format(status, category) not in metrics:
+                metrics["application_status_{}_{}_count".format(status, category)] = {"value": 0, "ts": timestamp}
+
+    return jsonify(metrics)
+
+
+@main.route('/metrics/applications.csv', methods=['GET'])
+def get_application_metrics_csv():
+    metrics = json.loads(get_application_metrics().data)
+    timestamp = pendulum.now().to_iso8601_string()
+    si = StringIO.StringIO()
+
+    writer = csv.writer(si, delimiter=',', quotechar='"')
+    writer.writerow(metrics.keys())
+    writer.writerow([timestamp]+[x['value'] for x in metrics.values()])
+
+    output = make_response(si.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename=application_metrics.csv"
+    output.headers["Content-type"] = "text/csv"
+    return output
+
+
 @main.route('/metrics/history', methods=['GET'])
 def get_historical_metrics():
     metrics = {}
@@ -41,7 +100,7 @@ def get_historical_metrics():
         .order_by(brief_day)\
         .group_by(brief_day)
     for (day, count) in db.session.execute(briefs_by_day):
-        print metrics["briefs_total_count"].append({"value": count, "ts": pendulum.instance(day).to_iso8601_string()})
+        metrics["briefs_total_count"].append({"value": count, "ts": pendulum.instance(day).to_iso8601_string()})
 
     metrics["brief_response_count"] = []
     brief_responses_day = func.date_trunc('day', BriefResponse.created_at)
@@ -49,7 +108,7 @@ def get_historical_metrics():
         .order_by(brief_responses_day) \
         .group_by(brief_responses_day)
     for (day, count) in db.session.execute(brief_responses_by_day):
-        print metrics["brief_response_count"].append({"value": count, "ts": pendulum.instance(day).to_iso8601_string()})
+        metrics["brief_response_count"].append({"value": count, "ts": pendulum.instance(day).to_iso8601_string()})
 
     metrics["buyer_count"] = []
     buyer_day = func.date_trunc('day', User.created_at)
@@ -60,7 +119,7 @@ def get_historical_metrics():
         .order_by(buyer_day)\
         .group_by(buyer_day)
     for (day, count) in db.session.execute(buyers_by_day):
-        print metrics["buyer_count"].append({"value": count, "ts": pendulum.instance(day).to_iso8601_string()})
+        metrics["buyer_count"].append({"value": count, "ts": pendulum.instance(day).to_iso8601_string()})
 
     metrics["supplier_count"] = []
     supplier_day = func.date_trunc('day', Supplier.creation_time)
@@ -69,6 +128,6 @@ def get_historical_metrics():
         .order_by(supplier_day) \
         .group_by(supplier_day)
     for (day, count) in db.session.execute(suppliers_by_day):
-        print metrics["supplier_count"].append({"value": count, "ts": pendulum.instance(day).to_iso8601_string()})
+        metrics["supplier_count"].append({"value": count, "ts": pendulum.instance(day).to_iso8601_string()})
 
     return jsonify(metrics)
