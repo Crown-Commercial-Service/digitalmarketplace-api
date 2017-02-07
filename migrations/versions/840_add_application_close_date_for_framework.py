e2ecb299acf6e2fa76e1fc199e02d141c4dbde7a
benvand <benjaminfvandersteen@gmail.com>
2017-02-07 10:41:21 +0000

    138323285-Add_application_close_date_to_framerwork
    * Migration

---
+++ migrations/versions/840_add_application_close_date_for_framework.py
@@ -0,0 +1,48 @@
+"""Add `application_close_date` for Framework
+Revision ID: 840
+Revises: 830
+Create Date: 2017-02-06 11:09:26.852142
+"""
+from alembic import op
+from datetime import datetime, tzinfo
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '840'
+down_revision = '830'
+
+
+frameworks_table = sa.table(
+    'frameworks',
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('slug', sa.String, nullable=False, unique=True, index=True),
+    sa.Column('allow_declaration_reuse', sa.Boolean),
+    sa.Column('application_close_date', sa.DateTime)
+)
+
+
+def upgrade():
+    op.add_column('frameworks', sa.Column('application_close_date', sa.DateTime(), nullable=True))
+    op.add_column(
+        'frameworks',
+        sa.Column('allow_declaration_reuse', sa.Boolean(), nullable=False, server_default='false')
+    )
+
+    fields = ('slug', 'application_close_date', 'allow_declaration_reuse')
+    new_values = (
+        ('digital-outcomes-and-specialists', datetime(2016, 1, 1, 15, tzinfo=tzinfo('utc')), False),
+        ('digital-outcomes-and-specialists-2', datetime(2017, 1, 16, 17, tzinfo=tzinfo('utc')), True),
+        ('g-cloud-8', datetime(2016, 6, 1, 17, tzinfo=tzinfo('utc')), True),
+
+    )
+    new_values = [dict(zip(fields, i)) for i in new_values]
+    for i in new_values:
+        op.execute(
+            frameworks_table.update().where(frameworks_table.c.slug==i.pop('slug')).values(**i)
+        )
+
+
+def downgrade():
+    op.drop_column('frameworks', 'allow_declaration_reuse')
+    op.drop_column('frameworks', 'application_close_date')
