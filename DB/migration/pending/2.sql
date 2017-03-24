dffc30f8286dc65b34cf700074a911dba0f8a063
Alexander Sadleir <maxious@users.noreply.github.com>
2017-03-24 11:09:08 +1100

    Update 2.sql

--- DB/migration/pending/2.sql
+++ DB/migration/pending/2.sql
@@ -1,1 +1,7 @@
-ALTER TYPE application_status_enum ADD VALUE 'deleted';
+alter table "public"."application" alter column "status" set data type varchar;
+
+drop type "public"."application_status_enum";
+
+create type "public"."application_status_enum" as enum ('saved', 'submitted', 'approved', 'complete', 'approval_rejected', 'assessment_rejected', 'deleted');
+
+alter table "public"."application" alter column "status" set data type application_status_enum using "status"::application_status_enum;
