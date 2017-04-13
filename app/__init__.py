d3ca395eedc8ebf8db6befdf3efc4e10c11c3253
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-04-13 16:29:48 +0100

    Change naming convention to match existing names
    After upgrading SQLAlchemy to 1.1 it seems that migrations stopped
    generating predictable names, so a naming convention was introduced. See
    here: [https://github.com/alphagov/digitalmarketplace-api/pull/524](https://github.com/alphagov/digitalmarketplace-api/pull/524)
    
    This created an issue however if you are importing a db dump from
    prod/stating/preview to a local, upgraded db. The local db will have had
    all migrations applied using the new naming convention where as the dump
    won't have. The schemas won't match.
    
    When importing the dump the first thing it does is to drop all tables
    and constraints it knows about before it rebuilds them. This means it
    tries to drop constraints in the local db that don't exist, leading to
    errors.
    
    Changing the naming convention to match the original constraint name
    pattern prevents this.

--- app/__init__.py
+++ app/__init__.py
@@ -14,9 +14,9 @@ bootstrap = Bootstrap()
 db = SQLAlchemy(metadata=MetaData(naming_convention={
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s",
+    "ck": "%(constraint_name)s",
+    "fk": "%(table_name)s_%(column_0_name)s_fkey",
+    "pk": "%(table_name)s_pkey",
 }))
 search_api_client = dmapiclient.SearchAPIClient()
 feature_flags = flask_featureflags.FeatureFlag()
