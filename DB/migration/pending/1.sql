2e86fe882c1da0e5ef5835119169205cf3d59a4f
Alexander Sadleir <maxious@users.noreply.github.com>
2017-05-15 13:12:12 +1000

    Update 1.sql

--- DB/migration/pending/1.sql
+++ DB/migration/pending/1.sql
@@ -1,1 +1,9 @@
-alter table "public"."assessment" add column "active" boolean;
+alter table "public"."assessment" add column "active" boolean not null;
+
+alter table "public"."supplier_domain" drop constraint "supplier_domain_pkey";
+drop index if exists "public"."supplier_domain_pkey";
+alter table "public"."supplier_domain" alter column "domain_id" drop not null;
+alter table "public"."supplier_domain" alter column "id" set not null;
+alter table "public"."supplier_domain" alter column "supplier_id" drop not null;
+CREATE UNIQUE INDEX supplier_domain_pkey ON supplier_domain USING btree (id);
+alter table "public"."supplier_domain" add constraint "supplier_domain_pkey" PRIMARY KEY using index "supplier_domain_pkey";
