fb34a8c45836686e98bb05a1c281789d5799e3b3
matt-sm <matt-sm@users.noreply.github.com>
2017-08-18 15:22:39 +1000

    New db migration (#299)
    * WIP
    
    * wip
    
    * wip
    
    * new db migration
    
    * remove comments
    
    * fix target
    
    * remove schema stripping
    
    * do not pass connection config
    
    * force plugin install

--- db_tasks.py
+++ db_tasks.py
@@ -1,26 +1,26 @@
 from __future__ import unicode_literals, print_function
 
-import paramiko
-from sshtunnel import SSHTunnelForwarder
-
 import json
-import io
 import os
 import sys
 from base64 import b64decode
+import httplib
+import ssl
+import urllib
+import base64
 
 from sqlbag import S, load_sql_from_file, temporary_database as temporary_db, sql_from_folder, raw_execute, DB_ERROR_TUPLE
 from migra import Migration
 from contextlib import contextmanager
 import tempfile
 import shutil
-import subprocess
-
+import pickle
 
 from migrations import load_from_app_model
 
 
 PENDING_FOLDER = 'DB/migration/pending'
+SHELF_PATH = 'DB/migration/'
 DRY_RUN = False
 
 
@@ -35,6 +35,28 @@ def tempfolder():
             shutil.rmtree(t)
 
 
+def shelf_full_path(filename):
+    return os.path.join(SHELF_PATH, filename)
+
+
+def shelve_result(filename, result):
+    shelf_path = shelf_full_path(filename)
+
+    with open(shelf_path, "w") as shelf_file:
+        pickle.dump(result, shelf_file)
+
+
+def get_shelved_result(filename):
+    shelf_path = shelf_full_path(filename)
+    if not (os.access(shelf_path, os.R_OK)):
+        raise Exception('Migration test results not found.')
+
+    with open(shelf_path, "r") as shelf_file:
+        result = pickle.load(shelf_file)
+
+    return result
+
+
 def databases_are_equal(dburl_a, dburl_b):
     with S(dburl_a) as s0, S(dburl_b) as s1:
         m = Migration(s0, s1)
@@ -47,22 +69,50 @@ def databases_are_equal(dburl_a, dburl_b):
         return not m.statements
 
 
-def do_schema_dump(dburl, outfile):
-    COMMAND = 'pg_dump --no-owner --no-privileges --schema-only --column-inserts -f {} {}'
+def do_schema_dump(outfile, cld_url, export_url, username, password, service_binding):
+    ctx = ssl.create_default_context()
 
-    command = COMMAND.format(outfile, dburl)
+    print('GETTING TOKEN')
+    conn = httplib.HTTPSConnection(cld_url, 443, context=ctx)
+    conn.request("POST", "/oauth/token", urllib.urlencode({
+        "grant_type": "password",
+        "scope": ",".join(["openid", "cloud_controller.read"]),
+        "username": username,
+        "password": password,
+    }), {
+        "Authorization": "Basic %s" % base64.b64encode("%s:%s" % ("db-export-tool-api", "notasecret")),
+        "Content-Type": "application/x-www-form-urlencoded",
+    })
+    resp = conn.getresponse()
+    if resp.status != 200:
+        raise Exception('Could not get token from {}'.format(cld_url))
+    token = json.loads(resp.read())["access_token"]
+    conn.close()
 
-    print('MAKING DUMP OF SCHEMA: '.format(command))
-    subprocess.check_output(command, shell=True)
-    print('DUMP COMPLETE')
+    print('DOWNLOADING SCHEMA')
+    conn = httplib.HTTPSConnection(export_url, 443, context=ctx)
+    conn.request("GET", "/dbSchema?%s" % urllib.urlencode({
+        "servicebinding": service_binding,
+    }), None, {
+        "Authorization": "Bearer %s" % token,
+    })
+
+    resp = conn.getresponse()
+    if resp.status != 200:
+        raise Exception('Could not get schema from {}'.format(export_url))
+    schema = resp.read()
+    conn.close()
+
+    with open(outfile, "w") as schema_file:
+        schema_file.write(schema)
 
 
-def do_migration(REAL_DB_URL):
+def test_migration(shelf_filename, cld_url, export_url, username, password, service_binding):
     PENDING = sql_from_folder(PENDING_FOLDER)
 
     with tempfolder() as tempf:
         outfile = os.path.join(tempf, 'schemadump.sql')
-        do_schema_dump(REAL_DB_URL, outfile)
+        do_schema_dump(outfile, cld_url, export_url, username, password, service_binding)
 
         for i in range(len(PENDING) + 1):
             ATTEMPTING = list(reversed(PENDING))[:i]
@@ -77,25 +127,15 @@ def do_migration(REAL_DB_URL):
                     try:
                         for migration_sql in ATTEMPTING:
                             raw_execute(s_dummy, migration_sql)
-                    except DB_ERROR_TUPLE as e:
+                    except DB_ERROR_TUPLE:
                         print('TRIED USING LAST {} PENDING FILES TO MIGRATE BUT THIS FAILED, MOVING TO NEXT'.format(i))
                         continue
 
                 load_from_app_model(target_db_url)
 
                 if databases_are_equal(dummy_db_url, target_db_url):
-                    print('APPLYING LAST {} PENDING FILES'.format(i))
-
-                    with S(REAL_DB_URL) as s_real:
-                        for migration_sql in ATTEMPTING:
-                            if not DRY_RUN:
-                                print("EXECUTING:")
-                                print(migration_sql)
-                                raw_execute(s_real, migration_sql)
-                            else:
-                                print('DRY RUN, would apply:')
-                                print(migration_sql)
-                    print('SUCCESS: DATABASE UP TO DATE.')
+                    print('SUCCESS WITH LAST {} PENDING FILES'.format(i))
+                    shelve_result(shelf_filename, ATTEMPTING)
                     return 0
                 else:
                     print('TRIED USING LAST {} PENDING FILES TO MIGRATE BUT THIS DOES NOT GIVE A CORRECT OUTCOME, MOVING TO NEXT'.format(i))
@@ -104,35 +144,36 @@ def do_migration(REAL_DB_URL):
         return 1
 
 
-@contextmanager
-def connection_from_encoded_config(encoded_config):
-    CONFIG = json.loads(b64decode(encoded_config))
+def do_migration(shelf_filename):
+    REAL_DB_URL = os.environ['DATABASE_URL'].replace('reconnect=true', '')
 
-    pk = paramiko.RSAKey.from_private_key(file_obj=io.StringIO(CONFIG['private_key']))
+    ATTEMPTING = get_shelved_result(shelf_filename)
 
-    with SSHTunnelForwarder(
-        (CONFIG['remote_host'], 22),
-        ssh_username=CONFIG['user'],
-        ssh_pkey=pk,
-        remote_bind_address=(CONFIG['private_host'], 5432),
-        local_bind_address=('0.0.0.0', CONFIG['local_port'])
-    ):
-        yield CONFIG['connection_url']
+    print('APPLYING PENDING FILE')
+
+    with S(REAL_DB_URL) as s_real:
+        for migration_sql in ATTEMPTING:
+            if not DRY_RUN:
+                print("EXECUTING:")
+                print(migration_sql)
+                raw_execute(s_real, migration_sql)
+            else:
+                print('DRY RUN, would apply:')
+                print(migration_sql)
+    print('SUCCESS: DATABASE UP TO DATE.')
+    return 0
 
 
-def main(task_method, encoded_config):
-    with connection_from_encoded_config(encoded_config) as REAL_DB_URL:
-        result = task_method(REAL_DB_URL)
-    return result
+def main(task_method, args):
+    return task_method(*args)
+
 
 if __name__ == '__main__':
-    _, task_method_name, encoded_config = sys.argv
-
     try:
-        task_method = getattr(sys.modules[__name__], task_method_name)
+        task_method = getattr(sys.modules[__name__], sys.argv[1])
     except AttributeError:
         print('no such task')
         sys.exit(1)
 
-    result = main(task_method, encoded_config)
+    result = main(task_method, sys.argv[2:])
     sys.exit(result)
