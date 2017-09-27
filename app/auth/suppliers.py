7ee4996d3efd894b517e47f235ed309de9864a1f
Mark Dickie <mdickie1@gmail.com>
2017-09-27 11:06:31 +1000

    flatten supplier instance for get response (#319)
    unflatten posted json for instance update

--- app/auth/suppliers.py
+++ app/auth/suppliers.py
@@ -27,6 +27,9 @@ def update_supplier_details(supplier_code, **kwargs):
         raise ValueError("Unable to modify supplier. supplier with code {} does not exist".format(supplier_code))
 
     kwargs.pop('domains', None)
+
+    kwargs = revert_flattened_supplier_json(kwargs)
+
     supplier.update_from_json(kwargs)
 
     audit = AuditEvent(
@@ -43,5 +46,47 @@ def update_supplier_details(supplier_code, **kwargs):
     db.session.add(audit)
 
     db.session.commit()
+
+    return supplier
+
+
+def flatten_supplier(supplier):
+    addresses = supplier.get('addresses', None)
+    contacts = supplier.get('contacts', None)
+
+    if addresses is not None and len(addresses) > 0:
+        for k, v in addresses[0].items():
+            if k != 'id':
+                supplier['address_'+k] = v
+
+    if contacts is not None and len(contacts) > 0:
+        for k, v in contacts[0].items():
+            if k != 'id':
+                supplier['contact_'+k] = v
+
+    supplier.pop('addresses', None)
+    supplier.pop('address', None)
+    supplier.pop('contacts', None)
+
+    return supplier
+
+
+def revert_flattened_supplier_json(supplier):
+    supplier['addresses'] = [{}]
+    supplier.get('addresses')[0]['country'] = supplier.get('address_country', None)
+    supplier.get('addresses')[0]['address_line'] = supplier.get('address_address_line', None)
+    supplier.get('addresses')[0]['country'] = supplier.get('address_country', None)
+    supplier.get('addresses')[0]['postal_code'] = supplier.get('address_postal_code', None)
+    supplier.get('addresses')[0]['state'] = supplier.get('address_state', None)
+    supplier.get('addresses')[0]['suburb'] = supplier.get('address_suburb', None)
+    supplier.get('addresses')[0]['supplier_code'] = supplier.get('supplier_code', None)
+
+    supplier['contacts'] = [{}]
+    supplier.get('contacts')[0]['contactFor'] = supplier.get('contact_contactFor', None)
+    supplier.get('contacts')[0]['contact_for'] = supplier.get('contact_contact_for', None)
+    supplier.get('contacts')[0]['email'] = supplier.get('contact_email', None)
+    supplier.get('contacts')[0]['fax'] = supplier.get('contact_fax', None)
+    supplier.get('contacts')[0]['name'] = supplier.get('contact_name', None)
+    supplier.get('contacts')[0]['phone'] = supplier.get('contact_phone', None)
 
     return supplier
--- app/auth/views/suppliers.py
+++ app/auth/views/suppliers.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from flask_login import current_user, login_required
 from app.auth import auth
 from app.utils import get_json_from_request, json_has_required_keys
-from app.auth.suppliers import get_supplier, update_supplier_details, valid_supplier
+from app.auth.suppliers import get_supplier, update_supplier_details, valid_supplier, flatten_supplier
 
 
 @auth.route('/supplier', methods=['GET'])
@@ -12,6 +12,7 @@ def get_supplier_profile():
         return jsonify(message="Only users with role supplier can access supplier details"), 401
     else:
         supplier = get_supplier(current_user.supplier_code)
+        supplier = flatten_supplier(supplier.serializable)
         return jsonify(user=supplier), 200
 
 
@@ -23,8 +24,9 @@ def update_supplier_profile():
 
     try:
         json_payload = get_json_from_request()
-        updated_supplier = update_supplier_details(current_user.supplier_code, **json_payload)
-        return jsonify(user=updated_supplier), 200
+        supplier = update_supplier_details(current_user.supplier_code, **json_payload)
+
+        return jsonify(user=supplier), 200
 
     except Exception as error:
         return jsonify(message=error.message), 400
