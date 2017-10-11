4cf7cb54dfbe43345a263960dae4547e472a9a68
matt-sm <matt-sm@users.noreply.github.com>
2017-10-11 14:53:58 +1100

    add alert to catalog results (#328)

--- app/auth/views/services.py
+++ app/auth/views/services.py
@@ -1,14 +1,27 @@
-from datetime import datetime
 from flask import jsonify
-from flask_login import current_user, login_required
+from flask_login import login_required
 from collections import defaultdict
-from sqlalchemy import or_
 from app.auth import auth
 from app.utils import get_json_from_request, json_has_required_keys
 from app.models import (
-    db, Location, Region, ServiceCategory, ServiceType, ServiceTypePrice,
-    ServiceTypePriceCeiling, Supplier, ServiceSubType
+    db, Region, ServiceCategory, ServiceType, ServiceTypePrice,
+    Supplier, ServiceSubType
 )
+
+ALERTS = {
+    'fixed': {
+        'type': 'info',
+        'message': 'This service is a fixed fee - inclusive of travel, assessment and report'
+    },
+    'hourly': {
+        'type': 'warning',
+        'message': 'The prices for this service are per hour. Travel can be charged.'
+    },
+    'none': {
+        'type': 'warning',
+        'message': 'There are no services provided for that region.'
+    }
+}
 
 
 @auth.route('/regions', methods=['GET'])
@@ -42,11 +55,11 @@ def get_regions():
         state_regions_merged[main].add(tuple(i for i in [sub, sub_id]))
 
     region_data = [
-        {'mainRegion': main, 'subRegions': [{'name': sub[0], 'id': sub[1]} for sub in subs]}
-        for main, subs in state_regions_merged.items()
+        {'mainRegion': m, 'subRegions': [{'name': s[0], 'id': s[1]} for s in subs]}
+        for m, subs in state_regions_merged.items()
     ]
 
-    return jsonify(data=region_data), 200
+    return jsonify(regions=region_data), 200
 
 
 @auth.route('/services', methods=['GET'])
@@ -77,24 +90,29 @@ def get_category_services():
         sub_category_merged[main].add(tuple(i for i in [sub, sub_id]))
 
     services_data = [
-        {'mainCategory': main, 'subCategories': [{'serviceTypeName': sub[0], 'serviceTypeId': sub[1]} for sub in subs]}
-        for main, subs in sub_category_merged.items()
+        {'mainCategory': m, 'subCategories': [{'serviceTypeName': s[0], 'serviceTypeId': s[1]} for s in subs]}
+        for m, subs in sub_category_merged.items()
     ]
 
-    return jsonify(data=services_data), 200
+    return jsonify(categories=services_data), 200
 
 
 @auth.route('/seller-catalogue', methods=['POST'])
 @login_required
 def get_seller_catalogue_data():
     catalogue_json = get_json_from_request()
-    json_has_required_keys(catalogue_json, ['serviceTypeIds', 'regionId'])
+    json_has_required_keys(catalogue_json, ['serviceTypeId', 'regionId'])
+
+    service_type = db.session.query(ServiceType).get(catalogue_json['serviceTypeId'])
+
+    if service_type is None:
+        return jsonify(message='Invalid serviceTypeId'), 500
 
     price_supplier_query = (
         db.session.query(ServiceTypePrice, Supplier, ServiceSubType)
         .join(Supplier, ServiceTypePrice.supplier_code == Supplier.code)
         .outerjoin(ServiceSubType, ServiceTypePrice.sub_service_id == ServiceSubType.id).filter(
-            ServiceTypePrice.service_type_id.in_(catalogue_json['serviceTypeIds']),
+            ServiceTypePrice.service_type_id == catalogue_json['serviceTypeId'],
             ServiceTypePrice.region_id == catalogue_json['regionId']
         )
     )
@@ -120,8 +138,10 @@ def get_seller_catalogue_data():
         sub_category_merged[main].append(sub)
 
     price_data = [
-        {'category': main, 'services': [sub for sub in subs]}
-        for main, subs in sub_category_merged.items()
+        {'category': m, 'suppliers': [s for s in subs]}
+        for m, subs in sub_category_merged.items()
     ]
 
-    return jsonify(data=price_data), 200
+    alert = ALERTS['none'] if len(price_data) == 0 else ALERTS[service_type.fee_type.lower()]
+
+    return jsonify(alert=alert, categories=price_data), 200
