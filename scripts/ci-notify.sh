0395d4d050171e92ed6102a2bf6ae8cbd7cc59e6
Alex Sadleir <alex.sadleir@digital.gov.au>
2017-10-13 10:53:27 +1100

    add git log to slack release message [ci skip]

--- scripts/ci-notify.sh
+++ scripts/ci-notify.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+LASTTAG=$(git describe --abbrev=0 --tags HEAD~1)
+GITLOG=$(git log --no-merges --format="%cd %s" --date=short $LASTTAG...HEAD | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')
+PAYLOAD='{"channel": "#marketplace", "icon_emoji": ":lightning:","username": "releasebot",
+ "attachments": [ { "fallback": "A new API went live!",
+ "pretext": "A new API went live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'",
+ "text": "'"$GITLOG"'" }] }'
 
-curl -g -X POST --data-urlencode 'payload={"channel": "#marketplace", "username": "releasebot", "text": "A new Api went live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'", "icon_emoji": ":lightning:"}' "$SLACK_WEBHOOK_URL"
+echo $PAYLOAD
+curl -g -X POST --data-urlencode "payload=$PAYLOAD" "$SLACK_WEBHOOK_URL"
+
+ENVIRONMENT=production
+LOCAL_USERNAME=`whoami`
+REVISION=`git log -n 1 --pretty=format:"%H"`
+
+curl https://api.rollbar.com/api/1/deploy/ \
+  -F access_token=$ROLLBAR_TOKEN \
+  -F environment=$ENVIRONMENT \
+  -F revision=$REVISION \
+  -F local_username=$LOCAL_USERNAME
