10363884bb6bead38ffd3cdffd9821e0a642926b
Matt <matt.smith@digital.gov.au>
2017-10-16 14:40:34 +1100

    fix migration

--- DB/migration/pending/1.sql
+++ DB/migration/pending/1.sql
@@ -1,23 +1,3 @@
-create sequence "public"."service_sub_type_id_seq";
+alter table "public"."location" drop column "state";
 
-create table "public"."service_sub_type" (
-    "id" integer not null default nextval('service_sub_type_id_seq'::regclass),
-    "name" character varying not null,
-    "created_at" timestamp without time zone not null,
-    "updated_at" timestamp without time zone not null
-);
-
-
-alter table "public"."service_type" add column "fee_type" character varying;
-
-alter table "public"."service_type_price" add column "sub_service_id" integer;
-
-alter table "public"."service_type_price_ceiling" add column "sub_service_id" integer;
-
-CREATE UNIQUE INDEX service_sub_type_pkey ON service_sub_type USING btree (id);
-
-alter table "public"."service_sub_type" add constraint "service_sub_type_pkey" PRIMARY KEY using index "service_sub_type_pkey";
-
-alter table "public"."service_type_price_ceiling" add constraint "service_type_price_ceiling_sub_service_id_fkey" FOREIGN KEY (sub_service_id) REFERENCES service_sub_type(id);
-
-alter table "public"."service_type_price" add constraint "service_type_price_sub_service_id_fkey" FOREIGN KEY (sub_service_id) REFERENCES service_sub_type(id);
+alter table "public"."region" add column "state" character varying not null;
