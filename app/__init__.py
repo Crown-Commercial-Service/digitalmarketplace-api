2ddc8e574c2bd48452dc4c1d1af62901c8b7c695
Matt <matt.smith@digital.gov.au>
2017-10-18 16:40:48 +1100

    try to fix swagger spec route

--- app/__init__.py
+++ app/__init__.py
@@ -68,7 +68,7 @@ def create_app(config_name):
         'specs': [
             {
                 'endpoint': 'apispec_1',
-                'route': '/apispec_1.json',
+                'route': '/api/apispec_1.json',
                 'rule_filter': lambda rule: True,  # all in
                 'model_filter': lambda tag: True,  # all in
             }
