c64a060c59b1619c4e84763a19468b013090911d
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-11-23 14:25:57 +1100

    Load env variables from user-provided-service Create user provided service (done by hand in z-cld) Add routes to manifest Add service bindings to manifest Switch to govau branch of autopilot plugin

--- scripts/cf_run_app.sh
+++ scripts/cf_run_app.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
+eval $(./scripts/ups_as_envs.py)
 NEW_RELIC_CONFIG_FILE=newrelic.ini exec newrelic-admin run-program python wsgi.py
---
+++ scripts/ups_as_envs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import os
+import json
+import sys
+
+for ups in json.loads(os.environ.get("VCAP_SERVICES", "{}")).get("user-provided", []):
+    for k, v in ups.get("credentials", {}).items():
+        sys.stdout.write("export %s='%s'\n" % (k, v.replace("'", "'\\''")))
