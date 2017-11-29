fb59b556c26f93e9e2970e4f32580ff53fd7a263
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2017-11-29 16:23:31 +0000

    Migration to add new admin-framework-manager role

---
+++ migrations/versions/1060_add_framework_manager_role.py
@@ -0,0 +1,22 @@
+"""Add admin-framework-manager role
+
+Revision ID: 1060
+Revises: 1050
+Create Date: 2017-11-29 15:20:00.000000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1060'
+down_revision = '1050'
+
+from alembic import op
+
+
+def upgrade():
+    op.execute("COMMIT")  # See: http://stackoverflow.com/a/30910417/15720
+    op.execute("ALTER TYPE user_roles_enum ADD VALUE 'admin-framework-manager' AFTER 'admin-manager';")
+
+
+def downgrade():
+    raise NotImplemented("Cannot remove user role value")
