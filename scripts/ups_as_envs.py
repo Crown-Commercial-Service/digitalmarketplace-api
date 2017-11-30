dc93921d01115a587a5215fddd92860321431543
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-11-30 14:51:55 +1100

    Tentative fix for newlines

--- scripts/ups_as_envs.py
+++ scripts/ups_as_envs.py
@@ -6,4 +6,4 @@ import sys
 
 for ups in json.loads(os.environ.get("VCAP_SERVICES", "{}")).get("user-provided", []):
     for k, v in ups.get("credentials", {}).items():
-        sys.stdout.write("export %s='%s'\n" % (k, v.replace("'", "'\\''")))
+        sys.stdout.write('export %s="$(printf -- %s)"\n' % (k, repr(str(v))))
