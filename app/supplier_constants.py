b09636817da31f4fef47691c9c1f0c336b987fe5
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2018-01-23 15:22:33 +0000

    Patch supplier declaration with information from their account when a framework's state changes to pending

---
+++ app/supplier_constants.py
@@ -0,0 +1,15 @@
+# Here we define a set of hardcoded keys that we use when denormalizing data from Supplier/ContactInformation tables
+# into the SupplierFramework.declaration field. These are only used internally by the API - the frontends currently
+# do not need knowledge of them, so there is no reason to centralise them e.g. in utils at this time.
+KEY_DUNS_NUMBER = 'supplierDunsNumber'
+KEY_ORGANISATION_SIZE = 'supplierOrganisationSize'
+KEY_REGISTERED_NAME = 'supplierRegisteredName'
+KEY_REGISTRATION_BUILDING = 'supplierRegisteredBuilding'
+KEY_REGISTRATION_COUNTRY = 'supplierRegisteredCountry'
+KEY_REGISTRATION_DATE = 'supplierRegistrationDate'
+KEY_REGISTRATION_NUMBER = 'supplierCompanyRegistrationNumber'
+KEY_REGISTRATION_POSTCODE = 'supplierRegisteredPostcode'
+KEY_REGISTRATION_TOWN = 'supplierRegisteredTown'
+KEY_TRADING_NAME = 'supplierTradingName'
+KEY_TRADING_STATUS = 'supplierTradingStatus'
+KEY_VAT_NUMBER = 'supplierVatNumber'
