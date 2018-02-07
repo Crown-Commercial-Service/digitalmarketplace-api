3b68fd22c95c41f44295ccfdeb750fac79a49b89
George Lund <george.lund@digital.cabinet-office.gov.uk>
2018-02-07 16:40:54 +0000

    Add test of search API to reflect this app's dependency on it.
     - this is basically copy-pasta'd from the buyer app, we have a tech
       debt ticket already about resolving that.
    
    https://trello.com/c/K20ImpOe/94-api-status-should-reflect-search-api-dependency

--- app/status/views.py
+++ app/status/views.py
@@ -5,10 +5,11 @@ from . import status
 from . import utils
 from ..models import Framework
 from dmutils.status import get_flags
+from app import search_api_client
 
 
 @status.route('/_status')
-def status_no_db():
+def status():
 
     if 'ignore-dependencies' in request.args:
         return jsonify(
@@ -16,21 +17,48 @@ def status_no_db():
         ), 200
 
     version = current_app.config['VERSION']
+    apis = [
+        {
+            'name': 'Search API',
+            'key': 'search_api_status',
+            'status': search_api_client.get_status()
+        },
+    ]  # list for consistency with other apps - a chunk of this should move into utils?
 
-    try:
-        return jsonify(
-            status="ok",
-            frameworks={f.slug: f.status for f in Framework.query.all()},
-            version=version,
-            db_version=utils.get_db_version(),
-            flags=get_flags(current_app)
-        )
+    apis_with_errors = []
 
-    except SQLAlchemyError:
-        current_app.logger.exception('Error connecting to database')
-        return jsonify(
-            status="error",
-            version=version,
-            message="Error connecting to database",
-            flags=get_flags(current_app)
-        ), 500
+    for api in apis:
+        if api['status'] is None or api['status']['status'] != "ok":
+            apis_with_errors.append(api['name'])
+
+    # if no errors found, return as is.  Else, return an error and a message
+    if not apis_with_errors:
+        try:
+            return jsonify(
+                {api['key']: api['status'] for api in apis},
+                status="ok",
+                frameworks={f.slug: f.status for f in Framework.query.all()},
+                version=version,
+                db_version=utils.get_db_version(),
+                flags=get_flags(current_app)
+            )
+        except SQLAlchemyError:
+            current_app.logger.exception('Error connecting to database')
+            return jsonify(
+                status="error",
+                version=version,
+                message="Error connecting to database",
+                flags=get_flags(current_app)
+            ), 500
+
+    message = "Error connecting to {}.".format(
+        ", ".join(apis_with_errors)
+    )
+
+    return jsonify(
+        {api['key']: api['status'] for api in apis},
+        status="error",
+        version=version,
+        message=message,
+        flags=get_flags(current_app)
+    ), 500
