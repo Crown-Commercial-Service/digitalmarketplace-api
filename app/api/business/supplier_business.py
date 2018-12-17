d8189338c403f6decfaef46f8cf834438b4efff7
malmas18 <29763502+malmas18@users.noreply.github.com>
2018-12-17 15:19:25 +1100

    [MAR-2739] Add notification that changes are not submitted (#574)
    * add warning

--- app/api/business/supplier_business.py
+++ app/api/business/supplier_business.py
@@ -1,18 +1,29 @@
+import collections
 from app.api.business.validators import SupplierValidator
 from app.api.services import suppliers, application_service
-from flask import current_app
 
 
 def get_supplier_messages(code, skip_application_check):
+    applications = application_service.find(
+        supplier_code=code,
+        type='edit'
+    ).all()
+
+    if any([a for a in applications if a.status == 'saved']):
+        validation_result = collections.namedtuple('Notification', ['warnings', 'errors'])
+        return validation_result(warnings=[{
+            'message': 'You have changes saved in your seller profile. '
+                       'Please ensure you go to "Preview and update" and then "Submit updates" '
+                       'in your seller profile to finalise your changes, or you can '
+                       '"Discard all updates".',
+            'severity': 'warning',
+            'step': 'start'
+        }], errors=[])
+
     supplier = suppliers.get_supplier_by_code(code)
-
+    validation_result = SupplierValidator(supplier).validate_all()
     if skip_application_check is False:
-        application = application_service.find(
-            supplier_code=code,
-            status='submitted'
-        ).one_or_none()
-
-        if application:
+        if any([a for a in applications if a.status == 'submitted']):
             return None
 
-    return SupplierValidator(supplier).validate_all()
+    return validation_result
