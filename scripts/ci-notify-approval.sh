7ea6ac9b089278ca51eb422f59254b53140aa7fa
malmas18 <29763502+malmas18@users.noreply.github.com>
2019-03-21 15:58:21 +1100

    Circleci hold (#610)
    * add ready to go live message

--- scripts/ci-notify-approval.sh
+++ scripts/ci-notify-approval.sh
@@ -2,19 +2,13 @@
 LASTTAG=$(git describe --abbrev=0 --tags HEAD~1)
 GITLOG=$(git log --no-merges --format="%cd %s" --date=short $LASTTAG...HEAD | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g' | sed 's/\"/\\"/g')
 PAYLOAD='{"channel": "#marketplace", "icon_emoji": ":lightning:","username": "releasebot",
- "attachments": [ { "fallback": "A new API went live!",
- "pretext": "A new API went live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'",
+ "attachments": [ { "fallback": ":rotating_light: API is ready to go live!",
+ "pretext": ":rotating_light: API is ready to go live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'",
  "text": "'"$GITLOG"'" }] }'
 
 echo $PAYLOAD
-curl -g -X POST --data-urlencode "payload=$PAYLOAD" "$SLACK_WEBHOOK_URL"
+curl -g -X POST --data-urlencode "payload=$PAYLOAD" "$SLACK_MARKETPLACE_SUPPORT_WEBHOOK_URL"
 
 ENVIRONMENT=production
 LOCAL_USERNAME=`whoami`
 REVISION=`git log -n 1 --pretty=format:"%H"`
-
-curl https://api.rollbar.com/api/1/deploy/ \
-  -F access_token=$ROLLBAR_TOKEN \
-  -F environment=$ENVIRONMENT \
-  -F revision=$REVISION \
-  -F local_username=$LOCAL_USERNAME
