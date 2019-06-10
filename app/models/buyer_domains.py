7be0f6e5404d5683cd6eb0a5e0fdc7b7f6e36dcc
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-06-10 17:29:07 +0100

    BuyerEmailDomain: add unique constraint to domain_name to match migration state
    presumably this got missed off the model definition, but was included in
    the migrations and as such we are currently operating as though it were set

--- app/models/buyer_domains.py
+++ app/models/buyer_domains.py
@@ -5,7 +5,7 @@ class BuyerEmailDomain(db.Model):
     __tablename__ = 'buyer_email_domains'
 
     id = db.Column(db.Integer, primary_key=True)
-    domain_name = db.Column(db.String(), nullable=False)
+    domain_name = db.Column(db.String(), nullable=False, unique=True)
 
     def serialize(self):
         return {
