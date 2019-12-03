892205ab2da0ae7419b2fdebb36abe5c91350430
Sam <29763502+malmas18@users.noreply.github.com>
2019-12-04 09:22:47 +1100

    update tests

--- app/models.py
+++ app/models.py
@@ -2897,14 +2897,14 @@ class Application(db.Model):
                     self.data['labourHire'] = {}
                 else:
                     to_remove = []
-                    for state, state_value in self.get('labourHire', {}).iteritems():
+                    for state, state_value in self.data.get('labourHire', {}).iteritems():
                         if not (
                             state_value.get('expiry') and
                             state_value.get('licenceNumber')
                         ):
                             to_remove.append(state)
                     for r in to_remove:
-                        self.get('labourHire', {}).pop(r)
+                        self.data.get('labourHire', {}).pop(r)
 
             else:
                 supplier = Supplier()
