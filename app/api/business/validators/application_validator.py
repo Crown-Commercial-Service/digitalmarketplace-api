873e28149ba4caa2d125bde900da872073512d3a
Sam <29763502+malmas18@users.noreply.github.com>
2019-12-19 15:32:29 +1100

    fix validation

--- app/api/business/validators/application_validator.py
+++ app/api/business/validators/application_validator.py
@@ -230,19 +230,20 @@ class ApplicationValidator(object):
                     continue
                 licence_number = state_value.get('licenceNumber')
                 expiry = state_value.get('expiry')
-                if licence_number and not expiry:
-                    errors.append({
-                        'message': 'Please enter an expiry date for {}'.format(state_to_long_name(state)),
-                        'severity': 'error',
-                        'step': 'recruiter'
-                    })
+                if not licence_number or not expiry:
+                    if licence_number and not expiry:
+                        errors.append({
+                            'message': 'Please enter an expiry date for {}'.format(state_to_long_name(state)),
+                            'severity': 'error',
+                            'step': 'recruiter'
+                        })
 
-                if not licence_number and expiry:
-                    errors.append({
-                        'message': 'Please enter your licence number for {}'.format(state_to_long_name(state)),
-                        'severity': 'error',
-                        'step': 'recruiter'
-                    })
+                    if not licence_number and expiry:
+                        errors.append({
+                            'message': 'Please enter your licence number for {}'.format(state_to_long_name(state)),
+                            'severity': 'error',
+                            'step': 'recruiter'
+                        })
 
                 if expiry:
                     try:
