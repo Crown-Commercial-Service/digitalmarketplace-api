3470aaaf94efca8ccbb769dbcaabde2b294f3d72
Reshma Abraham <reshmamabraham@gmail.com>
2020-04-20 20:36:10 +1000

    adding abr api key

--- app/api/business/supplier_business.py
+++ app/api/business/supplier_business.py
@@ -7,7 +7,66 @@ from app.api.business.agreement_business import (get_current_agreement,
                                                  has_signed_current_agreement)
 from app.api.business.validators import SupplierValidator
 from app.api.services import application_service, key_values_service, suppliers
+import requests
+from requests.exceptions import (HTTPError, Timeout, ConnectionError, SSLError, ProxyError)
+import re
+from app.tasks import publish_tasks
+from app.api.business.errors import AbrError
+from flask import current_app
+import xml.etree.ElementTree as ElementTree
+import json
 
+def get_business_info_by_abn(email_address, abn):
+    apiKey = current_app.config['ABR_API_KEY']
+    includeHistoricalDetails = 'N'
+    abn = abn
+    url = 'https://abr.business.gov.au/abrxmlsearch/AbrXmlSearch.asmx/SearchByABNv201205?searchString=' + abn + '&includeHistoricalDetails=' + includeHistoricalDetails + '&authenticationGuid=' + apiKey
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        xmlText = response.content
+        root = ElementTree.fromstring(xmlText)
+
+    # Rasing Different exceptions
+    except ConnectionError as ex:
+        raise AbrError('Connection Error')
+
+    # Invalid HTTP Reponse
+    except HTTPError as ex:
+        raise AbrError('HTTP Error')
+
+    except ProxyError as ex:
+        raise AbrError('ProxyError')
+
+    except Timeout as ex:
+        raise AbrError('Timeout')
+
+    except SSLError as ex:
+        raise AbrError('SSLError')
+
+    # Any other expections
+    except Exception as ex:
+        raise AbrError('Some exception raised')
+
+    # takes the first organisationName
+    search_xml_organisation_name = re.findall(r'<organisationName>(.*?)</organisationName>', xmlText)
+    organisation_name = search_xml_organisation_name[0]
+
+    # takes the first postcode
+    search_xml_postcode = re.findall(r'<postcode>(.*?)</postcode>', xmlText)
+    postcode = search_xml_postcode[0]
+
+    # takes the first state
+    search_xml_state = re.findall(r'<stateCode>(.*?)</stateCode>', xmlText)
+    state = search_xml_state[0]
+
+    # a dict to store these pre-filled info
+    business_info_abn_dict = {'organisation_name': organisation_name, 'postcode':postcode, 'state': state}
+    business_info_abn = json.dumps(business_info_abn_dict)
+    print(business_info_abn_dict)
+
+    return business_info_abn
 
 def abn_is_used(abn):
     abn = "".join(abn.split())
