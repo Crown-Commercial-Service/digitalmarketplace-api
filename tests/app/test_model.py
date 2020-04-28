8342f2a093c542c85fb6f20a32d2874919edba0c
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-04-29 08:10:01 +1000

    Test labour hire data is retained for new seller applications

--- tests/app/test_model.py
+++ tests/app/test_model.py
@@ -1312,6 +1312,46 @@ class TestApplication(BaseApplicationTest):
             assert labour_hire == {}
 
     @mock.patch('app.jiraapi.JIRA')
+    @pytest.mark.parametrize('test_data', [
+        {
+            'recruiter': 'yes',
+            'labourHire': {
+                'qld': {'expiry': ''},
+                'sa': {'expiry': '2040-10-22', 'licenceNumber': '123'},
+                'vic': {'licenceNumber': ''}
+            }
+        },
+        {
+            'recruiter': 'both',
+            'labourHire': {
+                'qld': {'expiry': ''},
+                'sa': {'expiry': '2040-10-22', 'licenceNumber': '123'},
+                'vic': {'licenceNumber': ''}
+            }
+        }
+    ])
+    def test_labour_hire_data_is_retained_for_new_seller_application(self, jira, app, new_application,
+                                                                     applicant, test_data):
+        with app.app_context():
+            new_application.data['recruiter'] = test_data['recruiter']
+            new_application.data['labourHire'] = test_data['labourHire']
+
+            assert new_application.status == 'saved'
+            new_application.submit_for_approval()
+            assert new_application.status == 'submitted'
+            new_application.set_approval(approved=True)
+            assert new_application.status == 'approved'
+
+            labour_hire = new_application.data.get('labourHire')
+            assert labour_hire == {
+                'sa': {
+                    'expiry': '2040-10-22',
+                    'licenceNumber': '123'
+                }
+            }
+
+
+    @mock.patch('app.jiraapi.JIRA')
     def test_new_seller_application(self, jira, app):
         with app.test_request_context('/hello'):
             application = Application(data=INCOMING_APPLICATION_DATA)
