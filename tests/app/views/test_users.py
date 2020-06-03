5beb6e2f0883c433b2f002e3e68704e37846faa3
Reshma Abraham <reshmamabraham@gmail.com>
2020-06-03 10:24:28 +1000

    going back to the previous change

--- tests/app/views/test_users.py
+++ tests/app/views/test_users.py
@@ -6,7 +6,6 @@ from app import db, encryption
 from app.models import Address, User, Supplier, Application, Brief
 import pendulum
 from pendulum import create as datetime
-import abn
 from ..helpers import \
     BaseApplicationTest, \
     JSONTestMixin, \
