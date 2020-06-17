243a201dca34b0fc9cdde095ff4ea5d5b0fb118b
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-06-17 14:13:05 +1000

    Add conditions to restrict updates to seller document data

--- DB/migration/pending/1.sql
+++ DB/migration/pending/1.sql
@@ -2,7 +2,11 @@
 UPDATE supplier
 SET
     data = jsonb_set(data::jsonb, '{"documents","indemnity"}', data::jsonb -> 'documents' -> 'liability')::json,
-    last_update_time = current_timestamp;
+    last_update_time = current_timestamp
+WHERE 
+	status != 'deleted' and
+	data -> 'documents' is not null and
+	data -> 'documents' -> 'liability' is not null;
 
 UPDATE application
 SET
