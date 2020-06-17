c3b2a4f9583c8fff002b908cc32c5689630785ae
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-06-17 13:59:34 +1000

    Set updated at columns to current timestamp

--- DB/migration/pending/1.sql
+++ DB/migration/pending/1.sql
@@ -1,9 +1,13 @@
 -- Copy liability details over to indemnity so seller profiles pass validation
 UPDATE supplier
-SET data = jsonb_set(data::jsonb, '{"documents","indemnity"}', data::jsonb -> 'documents' -> 'liability')::json;
+SET
+    data = jsonb_set(data::jsonb, '{"documents","indemnity"}', data::jsonb -> 'documents' -> 'liability')::json,
+    last_update_time = current_timestamp;
 
 UPDATE application
-SET data = jsonb_set(data::jsonb, '{"documents","indemnity"}', data::jsonb -> 'documents' -> 'liability')::json
+SET
+    data = jsonb_set(data::jsonb, '{"documents","indemnity"}', data::jsonb -> 'documents' -> 'liability')::json,
+    updated_at = current_timestamp
 WHERE 
 	status in ('saved', 'submitted') and
 	data -> 'documents' is not null and
