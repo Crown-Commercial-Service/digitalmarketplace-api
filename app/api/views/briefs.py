34dfe2d89d98cffe9c89422c82f5ae73ee3194ca
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-07-27 11:22:05 +1000

    Don't send edit history for sellers that can't respond

--- app/api/views/briefs.py
+++ app/api/views/briefs.py
@@ -714,6 +714,8 @@ def get_opportunity_history(brief_id):
         not_found(e.message)
 
     edits['can_respond'] = can_respond
+    if user_role == 'supplier' and not can_respond:
+        edits['edits'] = []
 
     return jsonify(edits)
 
