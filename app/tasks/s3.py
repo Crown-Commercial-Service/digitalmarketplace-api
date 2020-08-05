6745b9e8a1529f890f271a5cbb8d9d0f3d53cc87
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-08-06 08:19:07 +1000

    Close BytesIO stream after writing file to zip

--- app/tasks/s3.py
+++ app/tasks/s3.py
@@ -86,6 +86,7 @@ def create_responses_zip(brief_id):
                     try:
                         bucket.download_fileobj(s3file, s3_stream)
                         zf.writestr(file['zip_name'], s3_stream.getvalue())
+                        s3_stream.close()
                     except botocore.exceptions.ClientError as e:
                         raise CreateResponsesZipException('The file "{}" failed to download'.format(s3file))
 
