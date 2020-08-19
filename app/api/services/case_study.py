3758fee55e0c57bf74d099a7be6eb07fe2d9122f
Reshma Abraham <reshmamabraham@gmail.com>
2020-08-19 23:27:12 +1000

    simplified case_study

--- app/api/services/case_study.py
+++ app/api/services/case_study.py
@@ -20,73 +20,12 @@ class CaseStudyService(Service):
             .subquery()
         )
 
-        cs_id = (
-            db.session.query(
-                CaseStudy.id.label('case_study_id')
-            )
-            .filter(
-                CaseStudy.supplier_code == supplier_code,
-                CaseStudy.status == 'approved',
-                CaseStudy.data['service'].astext == domain_name.c.name,
-            )
-            .subquery()
-        )
-
         case_study = (
-            db.session.query(
-                domain_name.c.name,
-                func.json_object_agg(cs_id.c.case_study_id,
-                                     func.json_build_object('approach',
-                                                            CaseStudy.data['client'].label('approach'),
-                                                            'client',
-                                                            CaseStudy.data['client'].label('client'),
-                                                            'opportunity',
-                                                            CaseStudy.data['opportunity'].label('opportunity'),
-                                                            'outcome',
-                                                            CaseStudy.data['outcome'].label('outcome'),
-                                                            'project_links',
-                                                            CaseStudy.data['project_links'].label('project_links'),
-                                                            'referee_contact',
-                                                            CaseStudy.data['referee_contact'].label('referee_contact'),
-                                                            'referee_email',
-                                                            CaseStudy.data['referee_email'].label('referee_email'),
-                                                            'referee_positon',
-                                                            CaseStudy.data['referee_position']
-                                                            .label('referee_position'),
-                                                            'referee_name',
-                                                            CaseStudy.data['referee_name'].label('referee_name'),
-                                                            'roles',
-                                                            CaseStudy.data['roles'].label('roles'),
-                                                            'timeframe',
-                                                            CaseStudy.data['timeframe'].label('timeframe'),
-                                                            'title',
-                                                            CaseStudy.data['title'].label('title')
-                                                            )
-                                     ).label('data')
-            )
+            db.session.query(CaseStudy.id.label('cs_id'), CaseStudy.data)
             .filter(CaseStudy.supplier_code == supplier_code,
                     CaseStudy.status == 'approved',
                     CaseStudy.data['service'].astext == domain_name.c.name,
                     )
-            .group_by(domain_name.c.name)
-            .subquery()
         )
 
-        case_studies_id_array = (
-            db.session.query(
-                func.array_agg(cs_id.c.case_study_id).label('case_study_id')
-            )
-            .subquery()
-        )
-
-        query = (
-            db
-            .session
-            .query(
-                case_studies_id_array.c.case_study_id,
-                domain_name.c.name.label('domain_name'),
-                case_study.c.data
-            )
-        )
-
-        return [case_study._asdict() for case_study in query.all()]
+        return [value._asdict() for value in case_study.all()]
