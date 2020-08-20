2e559e9d2938d851cdc679af64b9f78f06b7f265
Reshma Abraham <reshmamabraham@gmail.com>
2020-08-20 22:55:25 +1000

    added testto check errors for vic

--- tests/app/api/business/validators/supplier_validator/test_validate_recruiters.py
+++ tests/app/api/business/validators/supplier_validator/test_validate_recruiters.py
@@ -1,7 +1,7 @@
 from app.api.business.validators import SupplierValidator
 from app.models import Supplier
 
-def test_can_zero_errors_for_sa_past_date_and_no_licence_number():
+def test_get_no_errors_from_expiried_sa_and_empty_licence_number():
     supplier = Supplier(
         data={
             'recruiter': 'yes',
@@ -15,3 +15,18 @@ def test_can_zero_errors_for_sa_past_date_and_no_licence_number():
     errors = SupplierValidator(supplier).validate_recruiter()
 
     assert len(errors) == 0
+
+def test_can_get_error_for_no_vic_licence_number():
+    supplier = Supplier(
+        data={
+            'recruiter': 'yes',
+            'labourHire': {
+                'vic': {
+                    'expiry': '01/01/2050'
+                }
+            }
+        }
+    )
+    errors = SupplierValidator(supplier).validate_recruiter()
+
+    assert len(errors) == 1
