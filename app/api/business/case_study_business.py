a63f5bb5c3268460aab436e8792be5eb7364f65f
Reshma Abraham <reshmamabraham@gmail.com>
2020-08-20 12:18:38 +1000

    removed retrieving the first item of the list

--- app/api/business/case_study_business.py
+++ app/api/business/case_study_business.py
@@ -5,5 +5,4 @@ from app.api.services import (
 
 def get_approved_case_studies(supplier_code, domain_id):
     case_studies = case_study_service.get_approved_case_studies_by_supplier_code(supplier_code, domain_id)
-    # case_studies = case_studies[0]
     return case_studies
