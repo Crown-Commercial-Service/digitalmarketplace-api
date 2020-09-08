4d937c23f128c24de6fe4d2f06d309a7cbb4eec8
Reshma Abraham <reshmamabraham@gmail.com>
2020-09-09 09:32:01 +1000

    linting

--- app/api/business/validators/application_validator.py
+++ app/api/business/validators/application_validator.py
@@ -253,7 +253,6 @@ class ApplicationValidator(object):
                     continue
                 licence_number = state_value.get('licenceNumber')
                 expiry = state_value.get('expiry')
-
                 if licence_number and not expiry:
                     errors.append({
                         'message': 'Please enter an expiry date for {}'.format(state_to_long_name(state)),
@@ -271,6 +270,7 @@ class ApplicationValidator(object):
                 if expiry:
                     try:
                         expiry_date = pendulum.parse(expiry, tz='Australia/Sydney')
+
                         if now > expiry_date.date():
                             errors.append({
                                 'message': 'Your {} labour hire licence has expired.'.format(state_to_long_name(state)),
