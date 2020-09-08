7af82d51d7565201f3ffea8a4c20e70cc2ee639b
Reshma Abraham <reshmamabraham@gmail.com>
2020-09-08 10:48:57 +1000

    adding additonal conditions to ignore south australia

--- app/api/business/validators/application_validator.py
+++ app/api/business/validators/application_validator.py
@@ -249,7 +249,7 @@ class ApplicationValidator(object):
             labour_hire = self.application.data.get('labourHire', {})
             now = pendulum.now('Australia/Canberra').date()
             for state, state_value in labour_hire.iteritems():
-                if not state_value:
+                if not state_value or state == 'sa':
                     continue
                 licence_number = state_value.get('licenceNumber')
                 expiry = state_value.get('expiry')
