4f54d3cc2526e2f2cb895c2e246a438942c724b3
Reshma Abraham <reshmamabraham@gmail.com>
2020-09-08 16:39:26 +1000

    future proof license expiry dates

--- tests/app/api/business/validators/application_validator/test_validate_recruiter.py
+++ tests/app/api/business/validators/application_validator/test_validate_recruiter.py
@@ -1,5 +1,6 @@
 from app.api.business.validators import ApplicationValidator
 from app.models import Application
+from datetime import date, timedelta
 
 
 def test_can_get_errors_with_none_recruiter():
@@ -67,12 +68,14 @@ def test_get_no_errors_for_sa_past_date_and_no_licence_number():
 
 
 def test_can_get_error_for_no_licence_number():
+    expiry_date = date.today() + timedelta(days=10)
+    expiry = '{}-{}-{}'.format(expiry_date.year, expiry_date.month, expiry_date.day)
     application = Application(
         data={
             'recruiter': 'yes',
             'labourHire': {
                 'vic': {
-                    'expiry': '01/01/2350'
+                    'expiry': expiry
                 }
             }
         }
@@ -83,12 +86,14 @@ def test_can_get_error_for_no_licence_number():
 
 
 def test_get_no_errors_from_sa_empty_licence_number():
+    expiry_date = date.today() + timedelta(days=10)
+    expiry = '{}-{}-{}'.format(expiry_date.year, expiry_date.month, expiry_date.day)
     application = Application(
         data={
             'recruiter': 'yes',
             'labourHire': {
                 'sa': {
-                    'expiry': '01/01/2350'
+                    'expiry': expiry
                 }
             }
         }
@@ -131,12 +136,14 @@ def test_can_get_no_errors_for_sa_no_expiry():
 
 
 def test_valid_for_recruiter_and_labour_hire():
+    expiry_date = date.today() + timedelta(days=10)
+    expiry = '{}-{}-{}'.format(expiry_date.year, expiry_date.month, expiry_date.day)
     application = Application(
         data={
             'recruiter': 'yes',
             'labourHire': {
                 'vic': {
-                    'expiry': '01/01/2350',
+                    'expiry': expiry,
                     'licenceNumber': 'foobar-licence'
                 }
             }
--- tests/app/api/business/validators/supplier_validator/test_validate_recruiters.py
+++ tests/app/api/business/validators/supplier_validator/test_validate_recruiters.py
@@ -1,5 +1,6 @@
 from app.api.business.validators import SupplierValidator
 from app.models import Supplier
+from datetime import date, timedelta
 
 
 def test_get_no_errors_from_expired_sa_and_empty_licence_number():
@@ -19,12 +20,14 @@ def test_get_no_errors_from_expired_sa_and_empty_licence_number():
 
 
 def test_get_no_errors_from_sa_empty_licence_number():
+    expiry_date = date.today() + timedelta(days=10)
+    expiry = '{}-{}-{}'.format(expiry_date.year, expiry_date.month, expiry_date.day)
     supplier = Supplier(
         data={
             'recruiter': 'yes',
             'labourHire': {
                 'sa': {
-                    'expiry': '01/01/2350'
+                    'expiry': expiry
                 }
             }
         }
@@ -35,12 +38,14 @@ def test_get_no_errors_from_sa_empty_licence_number():
 
 
 def test_can_get_error_for_vic_licence_number():
+    expiry_date = date.today() + timedelta(days=10)
+    expiry = '{}-{}-{}'.format(expiry_date.year, expiry_date.month, expiry_date.day)
     supplier = Supplier(
         data={
             'recruiter': 'yes',
             'labourHire': {
                 'vic': {
-                    'expiry': '01/01/2350'
+                    'expiry': expiry
                 }
             }
         }
@@ -67,12 +72,14 @@ def test_can_get_no_errors_for_sa_no_expiry():
 
 
 def test_valid_for_recruiter_and_labour_hire_vic():
+    expiry_date = date.today() + timedelta(days=10)
+    expiry = '{}-{}-{}'.format(expiry_date.year, expiry_date.month, expiry_date.day)
     supplier = Supplier(
         data={
             'recruiter': 'yes',
             'labourHire': {
                 'vic': {
-                    'expiry': '01/01/2350',
+                    'expiry': expiry,
                     'licenceNumber': 'foobar-licence'
                 }
             }
