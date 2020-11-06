4f7c0877fece0779ec4ac371089ce744a081fb13
Reshma-M-A <reshmamabraham@gmail.com>
2020-11-06 14:05:38 +1100

    fixed error

--- app/api/services/evidence.py
+++ app/api/services/evidence.py
@@ -111,9 +111,12 @@ class EvidenceService(Service):
             .join(DomainCriteria, DomainCriteria.id == domain_criteria_id.c.dc_id.cast(Integer))
             .filter(Evidence.id == evidence_id)
         )
+
         evidence = {}
-        evidence_data = [evidence._asdict() for evidence in result.all()]
-        evidence['evidence'] = evidence_data
+        for values in result.all():
+            evidence_data = values._asdict()
+
+        evidence['evidence'] = [evidence_data]
 
         category_max_rate = {}
         for c_values in category_name_max_daily_rate.all():
