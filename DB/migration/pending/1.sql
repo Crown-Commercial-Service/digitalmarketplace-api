baef48a5b78be245fa3771f66fa446d1153a0b13
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-12-08 12:17:13 +1100

    Add Service Integration and Management domain and criteria

--- DB/migration/pending/1.sql
+++ DB/migration/pending/1.sql
@@ -1,31 +1,18 @@
-DO $$                  
-    BEGIN 
-		IF NOT EXISTS (
-			SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'domain_criteria' AND COLUMN_NAME = 'essential'
-		)
-		THEN
-			ALTER TABLE public.domain_criteria ADD COLUMN essential boolean;
-			UPDATE public.domain_criteria SET essential = false;
-			ALTER TABLE public.domain_criteria ALTER COLUMN essential SET NOT NULL;
-		END IF;
-	END;
-$$;
-
 INSERT INTO 
 	public.domain (id, name, ordering, price_minimum, price_maximum, criteria_needed)
 VALUES 
-	(20, 'Platforms integration', 17, 500, 2500, 4)
+	(21, 'Service Integration and Management', 18, 500, 2400, 6)
 ON CONFLICT (id) DO NOTHING;
 	
 INSERT INTO 
 	public.domain_criteria (id, name, domain_id, essential)
 VALUES 
-	(2001, 'Experience with the Information Security Manual (ISM) and other Government security frameworks in relation to large transformation programs and experience in managing a consortia within these government security frameworks.', 20, true),
-	(2002, 'Experience in leading large transformational programs that deliver strategic digital platforms and experience in providing ongoing management and support of these platforms.', 20, true),
-	(2003, 'Demonstrated ability to work with providers to enable a scalable, reusable platform that can support multiple, distinct use cases.', 20, false),
-	(2004, 'Experience in providing ongoing management, support and continual improvement of these platforms with clear transparency of services and associated SLAs.', 20, false),
-	(2005, 'Experience in establishing a strategic vendor partnership with the client (for example, by delivering strategic insight and aligning product development roadmaps with client needs).', 20, false),
-	(2006, 'Ability to participate in development of transformational strategy, implementation planning / execution and operating model changes required to optimise outcomes.', 20, false),
-	(2007, 'Ability to manage a consortia of vendors and integrators to deliver a holistic solution / service.', 20, false),
-	(2008, 'Demonstrated ability to support Australian Industry and Indigenous participation.', 20, false)
+	(2101, 'Demonstrated organisational capability to support the delivery of service integration and management services to large, security conscious enterprises, including qualified and security cleared staff and resources.', 21, true),
+	(2102, 'Demonstrated experienced in the delivery of SIAM services to medium to large enterprises in accordance with SIAM and ITIL processes or similar principles.', 21, true),
+	(2103, 'Demonstrated experience and ability to manage complex relationships between Service Providers including managing conflicts of interest.', 21, true),
+	(2104, 'Experience in the development, maintenance and ongoing support of service portfolios and catalogues, including services provided by other suppliers and internal business providers.', 21, true),
+	(2105, 'Ability to manage a consortia of suppliers, including suppliers, Service Providers and carriers to deliver seamless end to end services to client stakeholders and users.', 21, true),
+	(2106, 'The Service Manager and/or their staff must hold appropriate recognised industry qualifications and/or certifications pertaining to ICT service management, ICT governance and ICT security management.', 21, true),
+	(2107, 'The Service Manager should hold recognised industry certifications relevant to quality and risk management, health and safety, project and programme management, environmental systems, etc.', 21, false),
+	(2108, 'Ability to support Australian industry, indigenous enterprises and small-to-medium enterprises (including as subcontractors) in the supply chain.', 21, false)
 ON CONFLICT (id) DO NOTHING;
