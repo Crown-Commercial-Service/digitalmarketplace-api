decf43c859cd28b1d38c164a2766a1e462755664
Reshma-M-A <reshmamabraham@gmail.com>
2021-01-06 14:04:40 +1100

    wip

--- app/api/services/abr.py
+++ app/api/services/abr.py
@@ -30,81 +30,81 @@ class AbrService(Service):
     def __init__(self, *args, **kwargs):
         super(AbrService, self).__init__(*args, **kwargs)
 
-    # def fetch_data2(self, abn):
-    #     url = self.build_url(abn)
-    #     result = ''
-    #     try:
-    #         response = requests.get(url)
-    #         xml_data = self.get_response(response)
-    #         result = self.get_data2(xml_data)
-    #         return result
-    #     else:
-    #         return result
+    def fetch_data2(self, abn):
+        url = self.build_url(abn)
+        # result = ''
+        # try:
+        response = requests.get(url)
+        xml_data = self.get_response(response)
+        result = self.get_data2(xml_data)
+        return result
+        # else:
+        #     return result
 
-    # def build_url(self, abn):
-    #     api_key = current_app.config['ABR_API_KEY']
-    #     include_historical_details = 'N'
-    #     link = 'https://abr.business.gov.au/abrxmlsearch/AbrXmlSearch.asmx/SearchByABNv201205?searchString='
-    #     url = link + abn + '&includeHistoricalDetails=' + include_historical_details + '&authenticationGuid=' + api_key
-    #     return url
+    def build_url(self, abn):
+        api_key = current_app.config['ABR_API_KEY']
+        include_historical_details = 'N'
+        link = 'https://abr.business.gov.au/abrxmlsearch/AbrXmlSearch.asmx/SearchByABNv201205?searchString='
+        url = link + abn + '&includeHistoricalDetails=' + include_historical_details + '&authenticationGuid=' + api_key
+        return url
 
-    # def get_response(self, response):
-    #     if response.ok:
-    #         xmlText = response.content
-    #         return xmlText
-    #     # is this the right place to put this?
-    #     # this raises for 400 or 500 calls
-    #     response.raise_for_status()
+    def get_response(self, response):
+        if response.ok:
+            xmlText = response.content
+            return xmlText
+        # is this the right place to put this?
+        # this raises for 400 or 500 calls
+        response.raise_for_status()
 
-    # # Raising different exceptions
-    # # timeout is considered as payload exception hence why it is not included
-    #     except ConnectionError as ex:
-    #         raise AbrError('Connection Error')
+    # Raising different exceptions
+    # timeout is considered as payload exception hence why it is not included
+        except ConnectionError as ex:
+            raise AbrError('Connection Error')
 
-    #     except HTTPError as ex:
-    #         raise AbrError('HTTP Error')
+        except HTTPError as ex:
+            raise AbrError('HTTP Error')
 
-    #     except ProxyError as ex:
-    #         raise AbrError('ProxyError')
+        except ProxyError as ex:
+            raise AbrError('ProxyError')
 
-    #     except SSLError as ex:
-    #         raise AbrError('SSL Error')
+        except SSLError as ex:
+            raise AbrError('SSL Error')
 
-    #     except Exception as ex:
-    #             raise AbrError('Failed exception raised')
+        except Exception as ex:
+                raise AbrError('Failed exception raised')
 
-    # def get_data2(self, xmlText):
-    #     # check and see if xmlText contains organisation Name
-    #     if re.findall(r'<organisationName>(.*?)</organisationName>', xmlText):
-    #         # takes the first organisation name
-    #         search_xml_organisation_name = re.findall(r'<organisationName>(.*?)</organisationName>', xmlText)
-    #         organisation_name = search_xml_organisation_name[0]
-    #         # this only works for &, < and > but not ' and ""
-    #         organisation_name = saxutils.unescape(organisation_name)
+    def get_data2(self, xmlText):
+        # check and see if xmlText contains organisation Name
+        if re.findall(r'<organisationName>(.*?)</organisationName>', xmlText):
+            # takes the first organisation name
+            search_xml_organisation_name = re.findall(r'<organisationName>(.*?)</organisationName>', xmlText)
+            organisation_name = search_xml_organisation_name[0]
+            # this only works for &, < and > but not ' and ""
+            organisation_name = saxutils.unescape(organisation_name)
 
-    #         # takes the first postcode
-    #         search_xml_postcode = re.findall(r'<postcode>(.*?)</postcode>', xmlText)
-    #         postcode = search_xml_postcode[0]
+            # takes the first postcode
+            search_xml_postcode = re.findall(r'<postcode>(.*?)</postcode>', xmlText)
+            postcode = search_xml_postcode[0]
 
-    #         # takes the first state
-    #         search_xml_state = re.findall(r'<stateCode>(.*?)</stateCode>', xmlText)
-    #         state = search_xml_state[0]
+            # takes the first state
+            search_xml_state = re.findall(r'<stateCode>(.*?)</stateCode>', xmlText)
+            state = search_xml_state[0]
 
-    #         return json.dumps({
-    #             'organisation_name': organisation_name,
-    #             'postcode': postcode,
-    #             'state': state
-    #         })
+            return json.dumps({
+                'organisation_name': organisation_name,
+                'postcode': postcode,
+                'state': state
+            })
 
-    #     # Payload exceptions: https://abr.business.gov.au/Documentation/Exceptions
-    #     else:
-    #         search_exception_code = re.findall(r'<exceptionCode>(.*?)</exceptionCode>', xmlText)
-    #         exception_code = search_exception_code[0]
+        # Payload exceptions: https://abr.business.gov.au/Documentation/Exceptions
+        else:
+            search_exception_code = re.findall(r'<exceptionCode>(.*?)</exceptionCode>', xmlText)
+            exception_code = search_exception_code[0]
 
-    #         search_exception_description = re.findall(r'<exceptionDescription>(.*?)</exceptionDescription>', xmlText)
-    #         exception_description = search_exception_description[0]
+            search_exception_description = re.findall(r'<exceptionDescription>(.*?)</exceptionDescription>', xmlText)
+            exception_description = search_exception_description[0]
 
-    #         raise AbrError(exception_code + ': ' + exception_description)
+            raise AbrError(exception_code + ': ' + exception_description)
 
     # using the supplier's abn to set up the link to be sent ABR API
     def get_url(self, abn):
