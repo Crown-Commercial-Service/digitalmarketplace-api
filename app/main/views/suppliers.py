816b9357c649af10c9bac1e17ac28dc073422a51
Reshma-M-A <reshmamabraham@gmail.com>
2021-03-10 13:19:27 +1100

    removing repeative code

--- app/main/views/suppliers.py
+++ app/main/views/suppliers.py
@@ -441,19 +441,16 @@ def do_search(search_query, offset, result_count, new_domains, framework_slug):
     if seller_types_list is not None and 'recruiter' in seller_types_list:
         q = q.filter(Supplier.data['recruiter'].astext == 'yes')
         seller_types_list.remove('recruiter')
-        if len(seller_types_list) == 0:
-            seller_types_list = None
 
     if seller_types_list is not None and 'consultant_and_recruiter' in seller_types_list:
         q = q.filter(Supplier.data['recruiter'].astext == 'both')
         seller_types_list.remove('consultant_and_recruiter')
-        if len(seller_types_list) == 0:
-            seller_types_list = None
 
     if seller_types_list is not None and 'consultant' in seller_types_list:
         q = q.filter(Supplier.data['recruiter'].astext == 'no')
-        seller_types_list.remove('consultant')
-        if len(seller_types_list) == 0:
+        seller_types_list.remove('consultant')   
+    
+    if seller_types_list is not None and len(seller_types_list) == 0:
             seller_types_list = None
 
     if seller_types_list is not None:
