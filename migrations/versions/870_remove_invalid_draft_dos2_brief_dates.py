b9c2c8644e5c429feffa24fcae183bfc2ab121b1
Alex Smith <alex.smith@digital.cabinet-office.gov.uk>
2021-05-07 09:54:19 +0100

    Make queries compatible with SQLAlchemy 1.4
    In SQLAlchemy 1.4 queries now accept columns as positional expressions.[[1]]
    
    We used to pass them as a tuple which is no longer supported, so
    separate each column out into it's own argument. This has no change to
    the behaviour of the query.
    
    [1]: https://docs.sqlalchemy.org/en/14/changelog/migration_14.html#change-5284

--- migrations/versions/870_remove_invalid_draft_dos2_brief_dates.py
+++ migrations/versions/870_remove_invalid_draft_dos2_brief_dates.py
@@ -47,10 +47,8 @@ def upgrade():
             briefs_table.c.published_at == sa.null()
         )
     ).with_only_columns(
-        (
-            briefs_table.c.id,
-            briefs_table.c.data
-        )
+        briefs_table.c.id,
+        briefs_table.c.data
     )
     results = conn.execute(query).fetchall()
 
